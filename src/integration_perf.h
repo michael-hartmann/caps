6135e5e8b13115f7679734791ed578214e043c2f
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-02-09 19:35:07 +0100

    implement cache

--- src/integration_perf.h
+++ src/integration_perf.h
@@ -4,14 +4,24 @@
 #include "edouble.h"
 
 typedef struct {
+    edouble **cache;
+    int N,size;
+} gaunt_cache_t;
+
+typedef struct {
     int nu_max;
     int m2_max;
     edouble tau;
     edouble *cache_I;
+    gaunt_cache_t *cache_gaunt;
 } integration_perf_t;
 
 void casimir_integrate_perf_init(integration_perf_t *self, double nT, int lmax);
 void casimir_integrate_perf_free(integration_perf_t *self);
-void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, casimir_integrals_t *cint);
+void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, casimir_integrals_t *cint, gaunt_cache_t *cache);
+
+void cache_gaunt_init(gaunt_cache_t *cache, int lmax);
+void cache_gaunt_free(gaunt_cache_t *cache);
+edouble *cache_gaunt_get(gaunt_cache_t *cache, int n, int nu, int m);
 
 #endif
