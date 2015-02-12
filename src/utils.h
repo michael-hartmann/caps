d3cee41f8de5462a48c4e0ba04ba54e5dbeab177
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-02-12 10:26:33 +0100

    add macro to abort program when an error occures

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,8 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
+#define TERMINATE(...) do { fprintf(stderr, "fatal error: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, " (%s:%d)\n", __FILE__, __LINE__); exit(1); } while(0);
+
 double now(void);
 
 void set_defaut_error_handler(void (*f)(const char *));
