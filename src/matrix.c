9d0cd16b19944acb887fe7e13d588495c41c2629
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-02-13 14:37:16 +0100

    matrix_sign_t

--- src/matrix.c
+++ src/matrix.c
@@ -5,6 +5,9 @@
 #include "matrix.h"
 #include "edouble.h"
 
+MATRIX_ALLOC(matrix_sign, matrix_sign_t, sign_t);
+MATRIX_FREE (matrix_sign, matrix_sign_t);
+
 MATRIX_ALLOC(matrix_edouble, matrix_edouble_t, edouble);
 MATRIX_FREE (matrix_edouble, matrix_edouble_t);
 MATRIX_LOGDET    (matrix_edouble, matrix_edouble_t, edouble, fabsq, copysignq, sqrtq, logq);
@@ -13,3 +16,4 @@ MATRIX_ABSMAX    (matrix_edouble, matrix_edouble_t, edouble, fabsq);
 MATRIX_BALANCE   (matrix_edouble, matrix_edouble_t, edouble, fabsq);
 
 MATRIX_LOG_BALANCE(matrix_edouble, matrix_edouble_t, edouble, logq);
+MATRIX_EXP(matrix_edouble, matrix_edouble_t, expq);
