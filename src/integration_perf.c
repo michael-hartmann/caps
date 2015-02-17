8d104c2aa9a3ca5ca8ccd6745218b3beb9d63e57
Michael Hartmann <github@speicherleck.de>
2015-02-17 20:58:32 +0100

    adjust integration for m=0

--- src/integration_perf.c
+++ src/integration_perf.c
@@ -26,7 +26,7 @@ static void poly1(const int m, edouble p[])
 
 
 /* p must have size nu+1-2m */
-static void poly2(int nu, int m2, edouble p[])
+static void poly2(const int nu, const int m2, edouble p[])
 {
     /* m2 = 2*m
      * d^2m/dz^2m P_(nu)(1+z)
@@ -96,42 +96,6 @@ static inline edouble I(integration_perf_t *self, int nu, int m2)
     return v;
 }
 
-static void polydplm(edouble pl1[], edouble pl2[], int l1, int l2, int m)
-{
-    int k;
-
-    if(m == 0)
-    {
-        for(k = 1; k <= l1; k++)
-            pl1[k-1] = expq(lngamma(1+k+l1)-lngamma(1+k)-lngamma(1+l1-k)-lngamma(k)-(k-1)*LOG2)/2;
-
-        for(k = 1; k <= l2; k++)
-            pl2[k-1] = expq(lngamma(1+k+l2)-lngamma(1+k)-lngamma(1+l2-k)-lngamma(k)-(k-1)*LOG2)/2;
-    }
-    else
-    {
-        for(k = 0; k <= l1-m+1; k++)
-            pl1[k] = 0;
-        for(k = 0; k <= l2-m+1; k++)
-            pl2[k] = 0;
-
-        pl1[l1+1-m] += (l1-m+1)*expq(lngamma(2*l1+3)-lngamma(l1+2)-lngamma(l1-m+2)-(l1+1-m)*LOG2);
-        for(k = 0; k <= l1-m; k++)
-        {
-            edouble common = expq(lngamma(1+k+l1+m)-lngamma(1+k+m)-lngamma(1+k)-lngamma(1+l1-k-m)-k*LOG2);
-            pl1[k] += common*(pow_2(m)+m*(k-l1-1)-2.0*k*l1-2*k)/(m-l1+k-1);
-            pl1[k+1] -= common*(l1+1);
-        }
-
-        pl2[l2+1-m] += (l2-m+1)*expq(lngamma(2*l2+3)-lngamma(l2+2)-lngamma(l2-m+2)-(l2+1-m)*LOG2);
-        for(k = 0; k <= l2-m; k++)
-        {
-            edouble common = expq(lngamma(1+k+l2+m)-lngamma(1+k+m)-lngamma(1+k)-lngamma(1+l2-k-m)-k*LOG2);
-            pl2[k] += common*(pow_2(m)+m*(k-l2-1)-2.0*k*l2-2*k)/(m-l2+k-1);
-            pl2[k+1] -= common*(l2+1.);
-        }
-    }
-}
 
 void casimir_integrate_perf_init(integration_perf_t *self, double nT, int lmax)
 {
@@ -195,46 +159,88 @@ void casimir_integrate_perf_free(integration_perf_t *self)
 /* TODO: m = 0, check signs! */
 void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, casimir_integrals_t *cint)
 {
+    int nu,q;
+    sign_t sign_A, sign_B, sign_C, sign_D;
     edouble tau = self->tau;
     edouble lnLambda = casimir_lnLambda(l1, l2, m, NULL);
+    edouble log_A,log_B,log_C,log_D, *sum = NULL;
+    sign_t *signs_l1pl2p, *signs_l1pl2m, *signs_l1ml2p, *signs_l1ml2m;
 
     if(m == 0)
     {
-        sign_t sign;
-        edouble value;
-        edouble pm[] = {0,2,1}; /* z²+2z */
-        edouble pdpl1m[l1-m+2];
-        edouble pdpl2m[l2-m+2];
-        edouble pdpl1mpdpl2m[l1+l2-1];
-        edouble pmpdpl1mpdpl2m[l1+l2+1];
+        const int qmax_l1pl2p = gaunt_qmax(l1+1,l2+1,2);
+        const int qmax_l1pl2m = gaunt_qmax(l1+1,l2-1,2);
+        const int qmax_l1ml2p = gaunt_qmax(l1-1,l2+1,2);
+        const int qmax_l1ml2m = gaunt_qmax(l1-1,l2-1,2);
 
-        polydplm(pdpl1m,pdpl2m,l1,l2,m);
+        sum = xmalloc((qmax_l1pl2p+1)*sizeof(edouble));
 
-        polymult(pdpl1m, l1, pdpl2m, l2, pdpl1mpdpl2m);
-        polymult(pm, 3, pdpl1mpdpl2m, l1+l2-1, pmpdpl1mpdpl2m);
+        edouble *a_l1pl2p = cache_gaunt_get(self, l1+1,l2+1,2, &signs_l1pl2p);
+        edouble *a_l1pl2m = cache_gaunt_get(self, l1+1,l2-1,2, &signs_l1pl2m);
+        edouble *a_l1ml2p = cache_gaunt_get(self, l1-1,l2+1,2, &signs_l1ml2p);
+        edouble *a_l1ml2m = cache_gaunt_get(self, l1-1,l2-1,2, &signs_l1ml2m);
+        sign_t signs[4] = {1,1,1,1};
+        edouble log_Bn[4] = { -INFINITY, -INFINITY, -INFINITY, -INFINITY };
 
-        value = polyintegrate(pmpdpl1mpdpl2m, l1+l2+1, 0, tau, &sign);
+        if((l1-1) >= 2 && (l2-1) >= 2)
+        {
+            for(q = 0; q <= qmax_l1ml2m; q++)
+            {
+                nu = l1-1+l2-1-2*q;
+                sum[q] = a_l1ml2m[q]+I(self,nu,4);
+            }
+            log_Bn[0] = gaunt_log_a0(l1-1,l2-1,2) + logadd_ms(sum, signs_l1ml2m, qmax_l1ml2m+1, &signs[0]);
+        }
 
-        cint->lnB_TM   = cint->lnB_TE = lnLambda-tau+value;
-        cint->signB_TM = -MPOW(l2+1)*sign;
+        if((l1-1) >= 2)
+        {
+            for(q = 0; q <= qmax_l1ml2p; q++)
+            {
+                nu = l1-1+l2+1-2*q;
+                sum[q] = a_l1ml2p[q] + I(self,nu,4);
+            }
+            log_Bn[1] = gaunt_log_a0(l1-1,l2+1,2) + logadd_ms(sum, signs_l1ml2p, qmax_l1ml2p+1, &signs[1]);
+            signs[1] *= -1;
+        }
+
+        if((l2-1) >= 2)
+        {
+            for(q = 0; q <= qmax_l1pl2m; q++)
+            {
+                nu = l1+1+l2-1-2*q;
+                sum[q] = a_l1pl2m[q] + I(self,nu,4);
+            }
+            log_Bn[2] = gaunt_log_a0(l1+1,l2-1,2) + logadd_ms(sum, signs_l1pl2m, qmax_l1pl2m+1, &signs[2]);
+            signs[2] *= -1;
+        }
+
+        for(q = 0; q <= qmax_l1pl2p; q++)
+        {
+            nu = l1+1+l2+1-2*q;
+            sum[q] = a_l1pl2p[q] + I(self,nu,4);
+        }
+        log_Bn[3] = gaunt_log_a0(l1+1,l2+1,2) + logadd_ms(sum, signs_l1pl2p, qmax_l1pl2p+1, &signs[3]);
+
+        log_B = logadd_ms(log_Bn, signs, 4, &sign_B) - logq(2*l1+1) - logq(2*l2+1);
+
+        cint->lnB_TM   = cint->lnB_TE = lnLambda-tau+log_B;
+        cint->signB_TM = -MPOW(l2+1)*sign_B;
         cint->signB_TE = -cint->signB_TM;
+        TERMINATE(isinf(cint->lnB_TM), "lnB is inf, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
+        TERMINATE(isnan(cint->lnB_TM), "lnB is nan, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
 
-        cint->lnA_TM = cint->lnA_TE = -INFINITY;
-        cint->lnC_TM = cint->lnC_TE = -INFINITY;
-        cint->lnD_TM = cint->lnD_TE = -INFINITY;
-        cint->signA_TM = cint->signA_TE = +1;
-        cint->signC_TM = cint->signC_TE = +1;
-        cint->signD_TM = cint->signD_TE = +1;
+        cint->lnA_TE = cint->lnA_TM   = -INFINITY;
+        cint->signA_TM = cint->signA_TE = 1;
 
-        TERMINATE(isinf(cint->lnB_TM), "lnB is inf, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
-        TERMINATE(isnan(cint->lnB_TM), "lnB is nan, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
+        cint->lnC_TE = cint->lnC_TM   = -INFINITY;
+        cint->signC_TM = cint->signC_TE = 1;
+
+        cint->lnD_TE = cint->lnD_TM   = -INFINITY;
+        cint->signD_TM = cint->signD_TE = 1;
     }
     else
     {
-        int nu,q;
-        sign_t sign_A, sign_B, sign_C, sign_D;
         const edouble log_m = logq(m);
-        edouble log_A,log_B,log_C,log_D;
 
         const int qmax_l1l2   = gaunt_qmax(l1,  l2,  m);
 
@@ -248,7 +254,7 @@ void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, cas
         const int qmax_l1ml2p = gaunt_qmax(l1-1,l2+1,m);
         const int qmax_l1ml2m = gaunt_qmax(l1-1,l2-1,m);
 
-        edouble *sum = xmalloc((qmax_l1pl2p+1)*sizeof(edouble));
+        sum = xmalloc((qmax_l1pl2p+1)*sizeof(edouble));
 
         sign_t *signs_l1l2;
         edouble *a_l1l2 = cache_gaunt_get(self, l1,l2,m, &signs_l1l2);
@@ -259,7 +265,6 @@ void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, cas
         edouble *a_l1l2p = cache_gaunt_get(self, l1,l2+1,m, &signs_l1l2p);
         edouble *a_l1l2m = cache_gaunt_get(self, l1,l2-1,m, &signs_l1l2m);
 
-        sign_t *signs_l1pl2p, *signs_l1pl2m, *signs_l1ml2p, *signs_l1ml2m;
         edouble *a_l1pl2p = cache_gaunt_get(self, l1+1,l2+1,m, &signs_l1pl2p);
         edouble *a_l1pl2m = cache_gaunt_get(self, l1+1,l2-1,m, &signs_l1pl2m);
         edouble *a_l1ml2p = cache_gaunt_get(self, l1-1,l2+1,m, &signs_l1ml2p);
@@ -384,9 +389,6 @@ void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, cas
             log_D = logadd_ms(log_Dn, signs, 2, &sign_D) - logq(2*l1+1);
         }
 
-        xfree(sum);
-
-
         cint->lnA_TM   = cint->lnA_TE = 2*log_m+lnLambda-tau+log_A;
         cint->signA_TM = -MPOW(l2)*sign_A; /* - because Lambda(l1,l2,m) is negative */
         cint->signA_TE = -cint->signA_TM;
@@ -403,18 +405,20 @@ void casimir_integrate_perf(integration_perf_t *self, int l1, int l2, int m, cas
         cint->signD_TM = -MPOW(l2+1)*sign_D;
         cint->signD_TE = -cint->signD_TM;
 
-        TERMINATE(isinf(cint->lnA_TM), "lnA is inf, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
-        TERMINATE(isnan(cint->lnA_TM), "lnA is nan, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
+        TERMINATE(isinf(cint->lnA_TM), "lnA is inf, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
+        TERMINATE(isnan(cint->lnA_TM), "lnA is nan, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
 
-        TERMINATE(isinf(cint->lnB_TM), "lnB is inf, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
-        TERMINATE(isnan(cint->lnB_TM), "lnB is nan, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
+        TERMINATE(isinf(cint->lnB_TM), "lnB is inf, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
+        TERMINATE(isnan(cint->lnB_TM), "lnB is nan, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
 
-        TERMINATE(isinf(cint->lnC_TM), "lnC is inf, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
-        TERMINATE(isnan(cint->lnC_TM), "lnC is nan, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
+        TERMINATE(isinf(cint->lnC_TM), "lnC is inf, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
+        TERMINATE(isnan(cint->lnC_TM), "lnC is nan, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
 
-        TERMINATE(isinf(cint->lnD_TM), "lnD is inf, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
-        TERMINATE(isnan(cint->lnD_TM), "lnD is nan, l1=%d,l2=%d,m=%d,tau=%g", l1,l2,m,(double)tau);
+        TERMINATE(isinf(cint->lnD_TM), "lnD is inf, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
+        TERMINATE(isnan(cint->lnD_TM), "lnD is nan, l1=%d,l2=%d,m=%d,tau=%Lg", l1,l2,m,tau);
     }
+
+    xfree(sum);
 }
 
 edouble *cache_gaunt_get(integration_perf_t *self, int n, int nu, int m, sign_t **signs)
