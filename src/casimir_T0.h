154ee65eed6c3cc6dfb41ed45435a76aad7a5309
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-07-30 16:07:23 +0200

    add prototype for master function

--- src/casimir_T0.h
+++ src/casimir_T0.h
@@ -6,7 +6,9 @@
 void usage(FILE *stream);
 double integrand(double xi, double LbyR, int lmax, double precision);
 
+int master(int argc, char *argv[], int cores);
 int slave(MPI_Comm master_comm, int rank);
+
 int submit_job(int destination, MPI_Request *request, double *recv, int k, double xi, double LbyR, int lmax, double precision);
 int retrieve_job(MPI_Request *request, double *buf, int *index, double *value);
 
