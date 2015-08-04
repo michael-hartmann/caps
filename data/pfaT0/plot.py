da0a0fc630b1c1310bf0342ec45acc8a0a960dd0
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-08-04 08:56:08 +0200

    plot data

--- data/pfaT0/plot.py
+++ data/pfaT0/plot.py
@@ -4,13 +4,8 @@ from __future__ import division
 
 from pyx import *
 from glob import glob
-from math import pi,log
+from math import pi,log,log
 
-pfa = lambda x: -pi**3/720.*(x+1)/x**2
-
-theta1 = 1./3 - 20/pi**2
-theta2 = -4.52
-f = "y(x)=1+%.15g*x+%.15g*x**2*log(x)" % (theta1,theta2)
 
 def slurp(filename, data=[]):
     with open(filename, "r") as f:
@@ -20,47 +15,80 @@ def slurp(filename, data=[]):
                 continue
             # L/R, lmax, order, alpha, F(T=0)
             LbyR,lmax,order,alpha,F = map(float, line.split(","))
-            data.append((LbyR, F, F/pfa(LbyR)))
+            ratio = F/pfa(LbyR)
+            data.append((LbyR, F, ratio, ratio/bimonte(LbyR)))
 
     return data
 
 
+if __name__ == "__main__":
+    # use LaTeX for Pyx
+    text.set(mode = "latex")
 
-data = []
-for filename in glob("slurm-*.out"):
-    slurp(filename, data)
+    # PFA formula
+    pfa = lambda x: -pi**3/720.*(x+1)/x**2
+
+    # formula Bimonte
+    theta1,theta2 = 1./3 - 20/pi**2, -4.52
+    bimonte = lambda x: 1+theta1*x+theta2*x**2*log(x)
+
+    # read in data
+    data = []
+    for filename in glob("slurm-*.out"):
+        slurp(filename, data)
+
+    # sort data
+    data = sorted(data, key=lambda x: x[0])
+
+    # plot F/F_PFA and Bimonte
+    g = graph.graphxy(
+        width = 10,
+        x = graph.axis.log(title=r"$x=L/R$", max=0.2, min=0.005),
+        y = graph.axis.lin(title=r"$\mathcal{F}/\mathcal{F}_\mathrm{PFA}(T=0)$", min=0.8),
+        key=graph.key.key(pos="tr", dist=0.1)
+    )
+
+    g2 = g.insert(graph.graphxy(
+        width = 4,
+        xpos = 1.5,
+        ypos = 1,
+        x = graph.axis.lin(max=0.008, min=0.005),
+        y = graph.axis.lin(max=0.992, min=0.9875)
+    ))
 
 
-text.set(mode = "latex")
-g = graph.graphxy(
-    width = 10,
-    x = graph.axis.log(title=r"$x=L/R$", max=0.2, min=0.005),
-    y = graph.axis.lin(title=r"$\mathcal{F}/\mathcal{F}_\mathrm{PFA}(T=0)$", min=0.8),
-    key=graph.key.key(pos="tr", dist=0.1)
-)
+    attrs = [color.gradient.RedBlue]
+    g.plot(
+        graph.data.points(data, x=1, y=3, title="numerisch vs Bordag"),
+        [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
+    )
 
-g2 = g.insert(graph.graphxy(
-    width = 4,
-    xpos = 1.5,
-    ypos = 1,
-    x = graph.axis.lin(max=0.008, min=0.005),
-    y = graph.axis.lin(max=0.992, min=0.9875)
-))
+    f = "y(x)=1+%.15g*x+%.15g*x**2*log(x)" % (theta1,theta2)
+    g.plot(graph.data.function(f, title="Bimonte et al."))
+
+    g2.plot(
+        graph.data.points(data, x=1, y=3, title="numerisch"),
+        [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
+    )
+
+    g2.plot(graph.data.function(f))
+
+    g.writePDFfile("pfaT0.pdf")
 
 
-attrs = [color.gradient.RedBlue]
-g.plot(
-    graph.data.points(data, x=1, y=3, title="numerisch vs Bordag"),
-    [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
-)
+    # Plot (F/F_PFA) / Bimonte
+    g = graph.graphxy(
+        width = 10,
+        x = graph.axis.log(title=r"$x=L/R$", max=0.02, min=0.004),
+        y = graph.axis.lin(min=0.9995, max=1.0005, title=r"$\frac{\mathcal{F}(T=0)}{\mathcal{F}_\mathrm{PFA}(T=0)}/\left(1+\theta_1 x + \theta_2 x^2 \log x\right)$"),
+    )
 
-g.plot(graph.data.function(f, title="Bimonte et al."))
+    g.plot(
+        graph.data.points(data, x=1, y=4),
+        [graph.style.symbol(graph.style.symbol.circle, size=0.06, symbolattrs=attrs), graph.style.line()]
+    )
 
-g2.plot(
-    graph.data.points(data, x=1, y=3, title="numerisch"),
-    [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
-)
+    g.finish()
+    g.stroke(g.ygridpath(1), [style.linestyle.dashed])
 
-g2.plot(graph.data.function(f))
-
-g.writePDFfile("pfaT0.pdf")
+    g.writePDFfile("ratio.pdf")
