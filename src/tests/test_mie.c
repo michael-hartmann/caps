84eefa780bac8ff51b949db43af06ed56f2d9897
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-09-03 13:30:46 +0200

    add tests

--- src/tests/test_mie.c
+++ src/tests/test_mie.c
@@ -47,6 +47,16 @@ int test_mie(void)
     AssertAlmostEqual(&test, _mie_lnb_perf(40,0.01,&sign), -648.6664276814638);
     AssertAlmostEqual(&test, _mie_lnb_perf(4,0.01,&sign),  -52.95166526324419);
 
+    AssertAlmostEqual(&test, _mie_lnb_perf(100, 1,&sign), -865.65411651438956672257219858405454738);
+    AssertAlmostEqual(&test, _mie_lnb_perf(200, 1,&sign), -2003.2700169651353702728300966686213123);
+    AssertAlmostEqual(&test, _mie_lnb_perf(500, 1,&sign), -5915.3560152708959234646060460452676614);
+    AssertAlmostEqual(&test, _mie_lnb_perf(1000,1,&sign), -13210.098885515418147666770491500341304);
+    AssertAlmostEqual(&test, _mie_lnb_perf(1500,1,&sign), -21027.802162198797680563024091196856145);
+    AssertAlmostEqual(&test, _mie_lnb_perf(2000,1,&sign), -29185.185715593291537268801628970184306);
+    AssertAlmostEqual(&test, _mie_lnb_perf(4000,1,&sign), -63907.254888471476868081861216637606434);
+    AssertAlmostEqual(&test, _mie_lnb_perf(6000,1,&sign), -100722.02894046555937519142066187010733);
+    AssertAlmostEqual(&test, _mie_lnb_perf(8000,1,&sign), -138895.87750031775994991231816543644142);
+
     /* a_l */
     AssertAlmostEqual(&test, _mie_lna_perf(3,3,&sign), 1.692450306201961);
     AssertEqual(&test, sign, -1);
@@ -62,6 +72,16 @@ int test_mie(void)
     AssertAlmostEqual(&test, _mie_lna_perf(20,0.01,&sign),  -300.7209163862779);
     AssertAlmostEqual(&test, _mie_lna_perf(30,0.01,&sign),  -471.3445070668955);
     AssertAlmostEqual(&test, _mie_lna_perf(30,0.001,&sign), -611.8021993589887);
+
+    AssertAlmostEqual(&test, _mie_lna_perf(100, 1,&sign), -865.64416766125947723847038002565294466);
+    AssertAlmostEqual(&test, _mie_lna_perf(200, 1,&sign), -2003.2650296097250827819811723946036985);
+    AssertAlmostEqual(&test, _mie_lna_perf(500, 1,&sign), -5915.3540172801973222879731127707420165);
+    AssertAlmostEqual(&test, _mie_lna_perf(1000,1,&sign), -13210.097886016582816381723859188214382);
+    AssertAlmostEqual(&test, _mie_lna_perf(1500,1,&sign), -21027.801495754698520339005510266151145);
+    AssertAlmostEqual(&test, _mie_lna_perf(2000,1,&sign), -29185.185215718437245666170730719302820);
+    AssertAlmostEqual(&test, _mie_lna_perf(4000,1,&sign), -63907.254638502745089438029419051667513);
+    AssertAlmostEqual(&test, _mie_lna_perf(6000,1,&sign), -100722.02877381278699710526450383761738);
+    AssertAlmostEqual(&test, _mie_lna_perf(8000,1,&sign), -138895.87737532557472806993280328577996);
 
     return test_results(&test, stderr);
 }
