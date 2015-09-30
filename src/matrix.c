a7c68037bb9360734f965df3e5eb266a095500d2
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-09-30 13:12:40 +0200

    support different algorithms to calculate determinant

--- src/matrix.c
+++ src/matrix.c
@@ -1,10 +1,13 @@
 /**
  * @file   matrix.c
  * @author Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
- * @date   April, 2015
+ * @date   September, 2015
  * @brief  matrix functions
  */
 
+#ifdef USE_LAPACK
+#include <cblas.h>
+#endif
 #include <stdio.h>
 #include <string.h>
 
@@ -41,29 +44,86 @@ double matrix_edouble_logdet(matrix_edouble_t *M, matrix_sign_t *M_sign, const c
         return matrix_edouble_logdet_lu(M);
     }
     #ifdef USE_LAPACK
-    else if(strcasecmp(type, "block") == 0)
+    else if(strcasecmp(type, "block_LAPACK") == 0)
     {
-        TERMINATE(0, "Not implemented: %s.", type);
-
         matrix_edouble_log_balance(M);
-        matrix_edouble_exp(M, M_sign);
-
-        return 0;
+        return matrix_logdet_block_lapack(M, M_sign);
     }
-    else if(strcasecmp(type, "LAPACK") == 0)
+    else if(strcasecmp(type, "LU_LAPACK") == 0)
     {
-        return matrix_logdet_lapack(M, M_sign);
+        matrix_edouble_log_balance(M);
+        return matrix_logdet_lu_lapack(M, M_sign);
     }
     #endif
     else
     {
-        TERMINATE(0, "Not implemented: %s.", type);
+        TERMINATE(0, "Algorithm not supported: %s.", type);
         return 0;
     }
 }
 
 #ifdef USE_LAPACK
-double matrix_logdet_lapack(matrix_edouble_t *M, matrix_sign_t *signs)
+static void matrix_mult(double *A, double *B, double *C, double alpha, double beta, int dim)
+{
+    char notrans = 'N';
+    dgemm_(
+        &notrans,  /* don't transpose/conjugate A */
+        &notrans,  /* don't transpose/conjugate B */
+        &dim,      /* M: rows of A and C */
+        &dim,      /* N: columns of B and C */
+        &dim,      /* K: columns of A and rows of B */
+        &alpha,    /* alpha: scalar */
+        A,         /* A: matrix A */
+        &dim,      /* LDA: leading dimension of A (columns) */
+        B,         /* B: matrix B */
+        &dim,      /* LDB: leading dimension of B (columns) */
+        &beta,     /* beta: scalar */
+        C,         /* C: matrix C */
+        &dim       /* LDC: leading dimension of C (columns) */
+    );
+}
+
+
+static int invert(double *M, int dim)
+{
+    int info, lwork;
+    int *ipiv = NULL;
+    double *work = NULL;
+    double workopt;
+
+    ipiv = xmalloc(dim*sizeof(int));
+
+    dgetrf_(
+        &dim, /* M number of rows of A */
+        &dim, /* N number of columns of A */
+        M,    /* matrix A to be factored */
+        &dim, /* LDA: leading dimension of A */
+        ipiv, /* pivot indices of dimension (min(M,N)) */
+        &info
+    );
+
+    lwork = -1;
+    dgetri_(&dim, M, &dim, ipiv, &workopt, &lwork, &info);
+    lwork = (int)workopt;
+    work = xmalloc(lwork*sizeof(double));
+
+    dgetri_(
+        &dim,   /* order of matrix A */
+        M,      /* factors L and U from LU decomposition */
+        &dim,   /* LDA: leading dimension of A */
+        ipiv,   /* pivot indices */
+        work,   /* workspace of dimension LWORK */
+        &lwork, /* length of work */
+        &info
+    );
+
+    xfree(ipiv);
+    xfree(work);
+
+    return info;
+}
+
+double matrix_logdet_lu_lapack(matrix_edouble_t *M, matrix_sign_t *signs)
 {
     const int dim = M->size;
     int i,j, m = dim, n = dim, lda = dim, info;
@@ -73,7 +133,7 @@ double matrix_logdet_lapack(matrix_edouble_t *M, matrix_sign_t *signs)
 
     for(i = 0; i < dim; i++)
         for(j = 0; j < dim; j++)
-            a[i+dim*j] = matrix_get(signs, i,j)*expe(matrix_get(M,i,j));
+            a[i+dim*j] = matrix_get(signs, i,j)*exp(matrix_get(M,i,j));
 
     dgetrf_(&m, &n, a, &lda, ipiv, &info);
 
@@ -81,6 +141,56 @@ double matrix_logdet_lapack(matrix_edouble_t *M, matrix_sign_t *signs)
         logdet += log(fabs(a[i+dim*i]));
 
     xfree(a);
+    xfree(ipiv);
+
+    return logdet;
+}
+
+double matrix_logdet_block_lapack(matrix_edouble_t *M, matrix_sign_t *signs)
+{
+    const int dim = M->size/2;
+    int i,j, m = dim, n = dim, lda = dim, info;
+    int *ipiv = xmalloc(dim*sizeof(int));
+    double *a = xmalloc(dim*dim*sizeof(double));
+    double *b = xmalloc(dim*dim*sizeof(double));
+    double *c = xmalloc(dim*dim*sizeof(double));
+    double *d = xmalloc(dim*dim*sizeof(double));
+    double *dinv = xmalloc(dim*dim*sizeof(double));
+    double logdet = 0;
+
+    for(i = 0; i < dim; i++)
+        for(j = 0; j < dim; j++)
+        {
+            a[i+dim*j] = matrix_get(signs, i,j)    *exp(matrix_get(M,i,j));
+            b[i+dim*j] = matrix_get(signs, i,j+dim)*exp(matrix_get(M,i,j+dim));
+            c[i+dim*j] = matrix_get(signs, i+dim,j)*exp(matrix_get(M,i+dim,j));
+            d[i+dim*j] = dinv[i+dim*j] = matrix_get(signs, i+dim,j+dim)*exp(matrix_get(M,i+dim,j+dim));
+        }
+
+    /* calculate logdet of matrix d */
+    dgetrf_(&m, &n, d, &lda, ipiv, &info);
+    for(i = 0; i < dim; i++)
+        logdet += log(fabs(d[i+dim*i]));
+
+    /* invert d */
+    invert(dinv, dim);
+
+    /* dinv*c -> d */
+    matrix_mult(dinv, c, d, 1, 0, dim);
+
+    /* a - b*d = a - b*dinv*c */
+    matrix_mult(b, d, a, -1, 1, dim);
+
+    /* calculate logdet of matrix a */
+    dgetrf_(&m, &n, a, &lda, ipiv, &info);
+    for(i = 0; i < dim; i++)
+        logdet += log(fabs(a[i+dim*i]));
+
+    xfree(a);
+    xfree(b);
+    xfree(c);
+    xfree(d);
+    xfree(dinv);
     xfree(ipiv);
 
     return logdet;
