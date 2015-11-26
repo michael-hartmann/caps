9fde08a5d810b9fd592abdec9afe327aaffaf1c9
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-11-26 16:41:43 +0100

    adjust to new API

--- src/tests/test_logdet.c
+++ src/tests/test_logdet.c
@@ -9,16 +9,14 @@ int test_logdet(void)
 {
     unittest_t test;
     casimir_t casimir;
-    integration_perf_t int_perf;
     double logdet;
 
     unittest_init(&test, "logdet M", "calculate logdet");
 
     casimir_init(&casimir, 0.006, 1.006);
     casimir_set_lmax(&casimir, 500);
-    casimir_integrate_perf_init(&int_perf, 1.006, 500);
 
-    logdet = casimir_logdetD(&casimir, 1, 1, &int_perf);
+    logdet = casimir_logdetD(&casimir, 1, 1);
     AssertAlmostEqual(&test, logdet, -9.27424711542347);
 
     casimir_free(&casimir);
