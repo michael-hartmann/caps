59418aa9b3c96f6f5a48d7f2c3c679392ca5fffa
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-12-01 15:28:12 +0100

    make job management simpler

--- src/casimir_T0.h
+++ src/casimir_T0.h
@@ -3,6 +3,27 @@
 
 #include <stdlib.h>
 
+typedef struct {
+    int index;
+    double xi;
+    double recv[1];
+    double value;
+    MPI_Request request;
+    int state;
+} casimir_task_t;
+
+typedef struct {
+    double LbyR, precision;
+    int lmax, cores;
+    casimir_task_t **tasks;
+} casimir_mpi_t;
+
+void casimir_mpi_init(casimir_mpi_t *self, double LbyR, int lmax, double precision, int cores);
+void casimir_mpi_free(casimir_mpi_t *self);
+int casimir_mpi_submit(casimir_mpi_t *self, int index, double xi);
+int casimir_mpi_retrieve(casimir_mpi_t *self, casimir_task_t **task_out);
+int casimir_mpi_get_running(casimir_mpi_t *self);
+
 void usage(FILE *stream);
 double integrand(double xi, double LbyR, int lmax, double precision);
 
