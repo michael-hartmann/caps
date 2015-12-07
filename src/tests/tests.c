4250d5873239e2e19acaa5179c99371b79f5d56c
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2015-12-07 12:41:02 +0100

    rearange tests

--- src/tests/tests.c
+++ src/tests/tests.c
@@ -18,17 +18,11 @@
 #include "test_Plm.h"
 #include "test_Xi.h"
 
-/* This makes perfectly sense even on a single core machine: This maybe won't
- * speedup things if you have less than CORES cores, but it will test, if
- * locking and multithreading is correct.
- */
-#define CORES 10
-
-
 int main(int argc, char *argv[])
 {
-    test_Lambda();
     test_lbinom();
+    test_Lambda();
+    test_Xi();
     //test_mie_drude();
 
     test_fresnel();
@@ -36,12 +30,12 @@ int main(int argc, char *argv[])
     test_Plm();
     test_doublefact();
     test_epsilon();
-    test_Xi();
-    test_integration_perf();
-    test_mie();
     test_besselI();
     test_besselK();
+    test_mie();
     test_determinant();
+
+    test_integration_perf();
 
     test_logdet();
 
