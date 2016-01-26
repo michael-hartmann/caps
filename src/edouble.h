e34d99cece3029696013b6dae9b96977cecaa641
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-01-26 10:38:49 +0100

    add types float64, float80 and float128 and math functions

--- src/edouble.h
+++ src/edouble.h
@@ -1,22 +1,46 @@
-#ifndef __EDOUBLE_H
-#define __EDOUBLE_H
+#ifndef __FLOATTYPES_H
+#define __FLOATTYPES_H
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
 
-#define edouble long double
+typedef double      float64;
+typedef long double float80;
+typedef __float128  float128;
 
-#define loge      logl
-#define expe      expl
-#define sqrte     sqrtl
-#define log1pe    log1pl
-#define fabse     fabsl
-#define sine      sinl
-#define cose      cosl
-#define gammae    gammal
-#define lgammae   lgammal
-#define copysigne copysignl
+#define log64      log
+#define exp64      exp
+#define sqrt64     sqrt
+#define log1p64    log1p
+#define fabs64     fabs
+#define sin64      sin
+#define cos64      cos
+#define gamma64    gamma
+#define lgamma64   lgamma
+#define copysign64 copysign
+
+#define log80      logl
+#define exp80      expl
+#define sqrt80     sqrtl
+#define log1p80    log1pl
+#define fabs80     fabsl
+#define sin80      sinl
+#define cos80      cosl
+#define gamma80    gammal
+#define lgamma80   lgammal
+#define copysign80 copysignl
+
+#define log128      logq
+#define exp128      expq
+#define sqrt128     sqrtq
+#define log1p128    log1pq
+#define fabs128     fabsq
+#define sin128      sinq
+#define cos128      cosq
+#define gamma128    gammaq
+#define lgamma128   lgammaq
+#define copysign128 copysignq
 
 #if defined(__clang__)
     #define COMPILER "clang/llvm"
