9a8710c5a6eeab38a884a9012e1a89efe7b28304
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-02-24 14:27:39 +0100

    use casimir_debug, add matrix_precondition

--- src/matrix.h
+++ src/matrix.h
@@ -301,6 +301,8 @@ double matrix_float80_logdet_qr(matrix_float80 *M);
 double matrix_float128_logdet_qr(matrix_float128 *M);
 #endif
 
+void matrix_precondition(matrix_float80 *A);
+
 void matrix_float80_log_balance(matrix_float80 *A, float80 *minimum, float80 *maximum);
 
 void matrix_float80_swap(matrix_float80 *M, const int i, const int j);
