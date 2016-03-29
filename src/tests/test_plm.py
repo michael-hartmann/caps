a7db1f0d8d22ba287798ac65ede1c202fc2edfd6
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-03-29 17:25:28 +0200

    Add more tests for Associated Legendre polynomials
    This commit adds a script to create tests for Associated Legendre polynomials
    and its derivatives. It also add the test.

--- src/tests/test_plm.py
+++ src/tests/test_plm.py
@@ -3,11 +3,7 @@
 from __future__ import division
 from mpmath import *
 
-mp.dps = 50
-
-def prettyprint(x, length=40):
-    print str(x)[:length]
-
+mp.dps = 100
 
 def Plm(l,m,x):
     if m % 2 == 0:
@@ -20,4 +16,35 @@ def dPlm(l,m,x):
 
 
 if __name__ == "__main__":
-    print(Plm(4,1,2)*Plm(3,1,2))
+    print("#include <math.h>")
+
+    print("#include \"unittest.h\"")
+    print("#include \"sfunc.h\"")
+    print("#include \"libcasimir.h\"")
+    print()
+    print("#include \"test_Plm.h\"")
+    print()
+    print("int test_Plm()")
+    print("{")
+    print("    sign_t sign;")
+    print("    unittest_t test;")
+    print()
+    print("    unittest_init(&test, \"Plm\", \"Test associated Legendre polynomials\");")
+    print()
+
+    for l in (1, 5, 10, 50, 100, 500, 1000, 2000, 5000):
+        for m in (0,1,5,10,50,100):
+            for x in (1.1, 2, 5, 10, 100, 1000, 1e6):
+                if l >= m:
+                    value = Plm(l,m,mpf(x))
+                    print("    AssertAlmostEqual(&test, plm_lnPlm(%d,%d,%g, &sign), %s);" % (l,m,x,float(log(abs(value)))))
+                    print("    AssertEqual(&test, sign, %d);" % sign(value))
+
+                    value = dPlm(l,m,mpf(x))
+                    print("    AssertAlmostEqual(&test, plm_lndPlm(%d,%d,%g, &sign), %s);" % (l,m,x,float(log(abs(value)))))
+                    print("    AssertEqual(&test, sign, %d);" % sign(value))
+                    print()
+
+    print()
+    print("    return test_results(&test, stderr);")
+    print("}")
