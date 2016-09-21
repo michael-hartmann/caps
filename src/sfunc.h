d27c2b1fde9c0881b5396afc1e7fd5c785b619be
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-09-21 12:13:45 -0300

    Use float64 to calculate Bessel functions

--- src/sfunc.h
+++ src/sfunc.h
@@ -43,9 +43,9 @@ inline float80 logadd_s(const float80 a, const sign_t sign_a, const float80 b, c
 inline float80 logadd_m(const float80 list[], const int len);
 inline float80 logadd_ms(const float80 list[], const sign_t signs[], const int len, sign_t *sign);
 
-float80 bessel_lnInu(const int n, const float64 x);
-float80 bessel_lnKnu(const int n, const float64 x);
-void bessel_lnInuKnu(int nu, const float64 x, float80 *lnInu_p, float80 *lnKnu_p);
+float64 bessel_lnInu(const int n, const float64 x);
+float64 bessel_lnKnu(const int n, const float64 x);
+void bessel_lnInuKnu(int nu, const float64 x, float64 *lnInu_p, float64 *lnKnu_p);
 
 float64 ln_doublefact(int n);
 
