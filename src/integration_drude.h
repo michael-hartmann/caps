7733d4b00dd56f885da32fa4280cc9079f10ddd4
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-09-21 13:23:49 -0300

    Get rid of float80

--- src/integration_drude.h
+++ src/integration_drude.h
@@ -12,20 +12,20 @@
 
 typedef struct {
     sign_t sign_A;
-    float80 lnA_TE;
-    float80 lnA_TM;
+    float64 lnA_TE;
+    float64 lnA_TM;
 
     sign_t sign_B;
-    float80 lnB_TE;
-    float80 lnB_TM;
+    float64 lnB_TE;
+    float64 lnB_TM;
     
     sign_t sign_C;
-    float80 lnC_TE;
-    float80 lnC_TM;
+    float64 lnC_TE;
+    float64 lnC_TM;
     
     sign_t sign_D;
-    float80 lnD_TE;
-    float80 lnD_TM;
+    float64 lnD_TE;
+    float64 lnD_TM;
 } integrands_drude_t;
 
 
@@ -48,7 +48,7 @@ struct integ_context {
     
     double         nT;
     int            l1, l2, m;
-    float80        c0, c_max;
+    float64        c0, c_max;
 };
 
 
@@ -59,8 +59,8 @@ void casimir_integrate_drude_free(integration_drude_t* int_drude);
 
 void casimir_integrate_drude(integration_drude_t* int_drude, int l1, int l2, casimir_integrals_t* cint);
 
-//void integrands_drude(float80 x, integrands_drude_t *integrands, casimir_t *self, double nT, int l1, int l2, int m);
+//void integrands_drude(float64 x, integrands_drude_t *integrands, casimir_t *self, double nT, int l1, int l2, int m);
 
-double log_polyintegrate(float80 p[], size_t len, int l1, int l2, int m, double tau, sign_t *sign);
+double log_polyintegrate(float64 p[], size_t len, int l1, int l2, int m, double tau, sign_t *sign);
 
 #endif
