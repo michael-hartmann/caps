230c060cc2973f6047d2abbaad2a093bf8ab04df
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-09-21 14:53:28 -0300

    Adjust to new functions; wip

--- src/python/libcasimir.pyx
+++ src/python/libcasimir.pyx
@@ -1,9 +1,7 @@
 import cython
-from longdouble cimport longdouble_c2py, longdouble_py2c
 import math
 from libcpp cimport bool
 
-ctypedef long double float80
 ctypedef signed char sign_t
 
 cdef extern from "libcasimir.h":
@@ -18,10 +16,9 @@ cdef extern from "libcasimir.h":
     int casimir_set_precision(casimir_t *self, double precision)
     void casimir_set_debug(casimir_t *self, bool debug)
     int casimir_set_cores(casimir_t *self, int cores)
-    int casimir_set_trace_threshold(casimir_t *self, double threshold)
     void casimir_set_verbose(casimir_t *self, bool verbose)
 
-    float80 casimir_lnLambda(int l1, int l2, int m, sign_t *sign)
+    double casimir_lnLambda(int l1, int l2, int m, sign_t *sign)
     double casimir_epsilon(double xi, double omegap, double gamma_)
 
     void casimir_lnab(casimir_t *self, const int n_mat, const int l, double *lna, double *lnb, sign_t *sign_a, sign_t *sign_b)
@@ -34,17 +31,17 @@ cdef extern from "libcasimir.h":
 
 
 cdef extern from "sfunc.h":
-    float80 logadd(const float80 a, const float80 b);
-    float80 logadd_s(const float80 a, const sign_t sign_a, const float80 b, const sign_t sign_b, sign_t *sign);
+    double logadd(const double a, const double b);
+    double logadd_s(const double a, const sign_t sign_a, const double b, const sign_t sign_b, sign_t *sign);
 
-    void bessel_lnInuKnu(int nu, const float80 x, float80 *lnInu_p, float80 *lnKnu_p);
-    float80 ln_doublefact(int n);
+    void bessel_lnInuKnu(int nu, const double x, double *lnInu_p, double *lnKnu_p);
+    double ln_doublefact(int n);
 
-    void bessel_lnInuKnu(int nu, const float80 x, float80 *lnInu_p, float80 *lnKnu_p);
+    void bessel_lnInuKnu(int nu, const double x, double *lnInu_p, double *lnKnu_p);
 
-    float80 gaunt_log_a0(int n, int nu, int m);
-    float80 gaunt_a0(int n,int nu,int m);
-    void gaunt(const int n, const int nu, const int m, float80 a_tilde[]);
+    double gaunt_log_a0(int n, int nu, int m);
+    double gaunt_a0(int n,int nu,int m);
+    void gaunt(const int n, const int nu, const int m, double a_tilde[]);
     int gaunt_qmax(const int n, const int nu, const int m);
 
 
@@ -52,31 +49,31 @@ cdef extern from "sfunc.h":
 class sfunc:
     def ln_doublefact(int n):
         """Calculate double factorial n!!."""
-        return longdouble_c2py( ln_doublefact(n) )
+        return ln_doublefact(n)
 
 
-    def logadd(a, b):
+    def logadd(double a, double b):
         """Return log(exp(a)+exp(b))."""
-        return longdouble_c2py( logadd(longdouble_py2c(a), longdouble_py2c(b)) )
+        return logadd(a, b)
 
 
-    def logadd_s(a, sign_a, b, sign_b):
+    def logadd_s(double a, sign_a, double b, sign_b):
         """Calculate v = sign_a*exp(a)+sign_b*exp(b) and return log|v| and sign(v)."""
-        cdef result
+        cdef double result
         cdef sign_t sign
         cdef sign_t sign_a_c = math.copysign(1, sign_a)
         cdef sign_t sign_b_c = math.copysign(1, sign_b)
 
-        result = logadd_s(longdouble_py2c(a), sign_a, longdouble_py2c(b), sign_b_c, &sign)
+        result = logadd_s(a, sign_a, b, sign_b_c, &sign)
 
-        return longdouble_c2py(result), <int>sign
+        return result, <int>sign
 
 
-    def bessel_lnInuKnu(int nu, x):
-        cdef float80 Inu, Knu
-        bessel_lnInuKnu(nu, longdouble_py2c(x), &Inu, &Knu)
+    def bessel_lnInuKnu(int nu, double x):
+        cdef double Inu, Knu
+        bessel_lnInuKnu(nu, x, &Inu, &Knu)
 
-        return longdouble_c2py(Inu), longdouble_c2py(Knu)
+        return Inu, Knu
 
 
     def bessel_lnInu(int nu, x):
@@ -91,7 +88,7 @@ class sfunc:
 cdef class Casimir:
     cdef casimir_t casimir
 
-    def __init__(self, LbyR=1, T=1, verbose=False, debug=False, omegap_sphere=None, gamma_sphere=None, omegap_plane=None, gamma_plane=None, lmax=None, cores=None, precision=None, trace_threshold=None):
+    def __init__(self, LbyR=1, T=1, verbose=False, debug=False, omegap_sphere=None, gamma_sphere=None, omegap_plane=None, gamma_plane=None, lmax=None, cores=None, precision=None):
         if LbyR <= 0:
             raise ValueError("LbyR must be positive")
         if T <= 0:
@@ -102,8 +99,6 @@ cdef class Casimir:
             raise ValueError("precision must be positive")
         if cores != None and (type(cores) != int or cores < 1):
             raise ValueError("cores must be a positive integer")
-        if trace_threshold != None and trace_threshold < 0:
-            raise ValueError("trace_threshold must be non negative")
 
         assert casimir_init(&(self.casimir), LbyR, T) == 0
 
@@ -115,8 +110,6 @@ cdef class Casimir:
             casimir_set_debug(&self.casimir, True)
         if cores:
             casimir_set_cores(&self.casimir, cores)
-        if trace_threshold:
-            casimir_set_trace_threshold(&self.casimir, trace_threshold)
         if verbose:
             casimir_set_verbose(&self.casimir, verbose)
 
@@ -124,38 +117,11 @@ cdef class Casimir:
 
     def lnLambda(Casimir self, int l1, int l2, int m):
         cdef sign_t sign
-        cdef float80 res = casimir_lnLambda(l1, l2, m, &sign)
-        return longdouble_c2py(res), <int>sign
+        cdef double result
+        result = casimir_lnLambda(l1, l2, m, &sign)
+        return result, <int>sign
 
 
-    def epsilon(Casimir self, double xi, double omegap, double gamma_):
-        return casimir_epsilon(xi, omegap, gamma_)
-
-
-    def lnepsilon(Casimir self, double xi, double omegap, double gamma_):
-        return casimir_lnepsilon(xi, omegap, gamma_)
-
-
-    def rp(Casimir self, nT, k):
-        raise NotImplementedError()
-
-
-    def F_SI_to_scaled(Casimir self, double F_SI, double ScriptL):
-        raise NotImplementedError()
-
-
-    def F_scaled_to_SI(Casimir self, double F_Sscaled, double ScriptL):
-        raise NotImplementedError()
-
-
-    def T_SI_to_scaled(Casimir self, double T_SI, double ScriptL):
-        raise NotImplementedError()
-
-    
-    def T_scaled_to_SI(Casimir self, double T_scaled, double ScriptL):
-        raise NotImplementedError()
-
-    
     def lnab(Casimir self, int n_mat, int l):
         cdef double lna, lnb
         cdef sign_t sign_a, sign_b
