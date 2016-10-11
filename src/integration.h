5662afe5cae4de0914d425c820a2e448bb984f8d
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-10-11 15:10:22 +0200

    Switch to simpler and hopefully faster integration
    Code is compiles, but is totally untested.

--- src/integration.h
+++ src/integration.h
@@ -23,18 +23,17 @@
 
 typedef struct {
     casimir_t *casimir;
-    int m;
+    int n,m;
     double nT,tau;
 } integration_t;
 
 typedef struct {
     double a,b;
-    sign_t signs[8];
     double K15[8];
     double err[8];
 } interval_t;
 
-void casimir_integrate_integrands(integration_t *int_obj, double z, int l1, int l2, double v[8]);
+void casimir_integrate_integrands(integration_t *int_obj, double t, int l1, int l2, double log_prefactor, double v[8]);
 
 int casimir_integrate_init(casimir_t *self, integration_t *int_obj, int n, int m);
 int casimir_integrate_free(integration_t *int_obj);
