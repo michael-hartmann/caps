5e0d673f493db3438c0f366895247eecb1a47ccc
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-11-30 14:54:51 +0100

    increase precision

--- src/libcasimir.c
+++ src/libcasimir.c
@@ -1388,7 +1388,7 @@ matrix_t *casimir_M(casimir_t *self, int n, int m)
     const size_t max = self->lmax;
     const size_t dim = (max-min+1);
 
-    integration_t *integration = casimir_integrate_init(self, n, m, 1e-6);
+    integration_t *integration = casimir_integrate_init(self, n, m, 1e-8);
 
     /* allocate space for matrix M */
     matrix_t *M = matrix_alloc(2*dim);
