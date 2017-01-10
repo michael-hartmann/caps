3247aceceda3d8865535023985387405f4dc97b4
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-01-10 09:54:26 +0100

    add dqage with irule=1

--- src/cquadpack/include/quadpack.h
+++ src/cquadpack/include/quadpack.h
@@ -1,6 +1,14 @@
 #ifndef QUADPACK_H
 #define QUADPACK_H
 
+#define GK_7_15   1
+#define GK_10_21  2
+#define GK_15_31  3
+#define GK_20_41  4
+#define GK_25_51  5
+#define GK_30_61  6
+
+
 /* DQAGI - Integration over (semi-) infinite intervals. (From QUADPACK)
  *
  *    Adaptive integration routine which handles functions
@@ -36,7 +44,8 @@
  *
  *    epsrel - relative accuracy requested.
  */
-double dqagi(double f(double, void *), double bound, int inf, double epsabs, double epsrel, double *abserr, int *neval, int *ier, void* user_data);
+double dqagi(double f(double, void *), double bound, int inf, double epsabs, double epsrel, double *abserr, int *neval, int *ier, void *user_data);
+
 
 /* DQAGS - Integration over finite intervals. (From QUADPACK)
  *
@@ -64,6 +73,35 @@ double dqagi(double f(double, void *), double bound, int inf, double epsabs, dou
  *
  *    epsrel - relative accuracy requested.
  */
-double dqags(double f(double, void *), double a, double b, double epsabs, double epsrel, double *abserr, int *neval, int *ier, void* user_data);
+double dqags(double f(double, void *), double a, double b, double epsabs, double epsrel, double *abserr, int *neval, int *ier, void *user_data);
+
+
+/* DQAGE - Approximation to definite integral. (From QUADPACK)
+ *
+ *    Allows user's choice of Gauss-Kronrod integration rule.
+ *
+ * PARAMETERS:
+ *
+ *    f() - double precision function to be integrated.
+ *
+ *    a - lower limit of integration.
+ *
+ *    b - upper limit of integration.
+ *
+ *    epsabs - absolute accuracy requested.
+ *
+ *    epsrel - relative accuracy requested.
+ *
+ *    irule - integration rule to be used as follows:
+ *        irule = 1 -- G_K 7-15
+ *        irule = 2 -- G_K 10-21
+ *        irule = 3 -- G_K 15-31
+ *        irule = 4 -- G_K 20-41
+ *        irule = 5 -- G_K 25-51
+ *        irule = 6 -- G_K 30-61
+ *
+ *    limit - maximum number of subintervals.
+ */
+double dqage(double f(double, void *), double a, double b, double epsabs, double epsrel, int irule, double *abserr, int *neval, int *ier, int *last, void *user_data);
 
 #endif
