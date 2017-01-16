1da6ac5e2f969fe8643a68980f5d091802632bd2
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-01-16 12:48:53 +0100

    fix wrapper so that it works again

--- src/python/libcasimir.pyx
+++ src/python/libcasimir.pyx
@@ -13,11 +13,10 @@ cdef extern from "matrix.h":
 cdef extern from "libcasimir.h":
     ctypedef struct casimir_t:
         double LbyR
-        double T
         double threshold
-        int lmax
+        int ldim
 
-    casimir_t *casimir_init(double LbyR, double T)
+    casimir_t *casimir_init(double LbyR)
     void casimir_free(casimir_t *self)
 
     void casimir_set_debug(casimir_t *self, bool debug)
@@ -28,41 +27,36 @@ cdef extern from "libcasimir.h":
 
     int casimir_set_epsilonm1(casimir_t *self, double (*epsilonm1)(double xi, void *userdata), void *userdata)
 
-    int casimir_get_lmax(casimir_t *self)
-    int casimir_set_lmax(casimir_t *self, int lmax)
+    int casimir_get_ldim(casimir_t *self)
+    int casimir_set_ldim(casimir_t *self, int ldim)
 
     detalg_t casimir_get_detalg(casimir_t *self)
-    int casimir_set_detalg(casimir_t *self, detalg_t detalg)
+    void casimir_set_detalg(casimir_t *self, detalg_t detalg)
 
-    double casimir_get_precision(casimir_t *self)
-    int    casimir_set_precision(casimir_t *self, double precision)
+    double casimir_get_tolerance(casimir_t *self);
+    int    casimir_set_tolerance(casimir_t *self, double tolerance);
 
     double casimir_lnLambda(int l1, int l2, int m)
 
-    void casimir_lnab0(int l, double *a0, sign_t *sign_a0, double *b0, sign_t *sign_b0)
-    void casimir_lnab(casimir_t *self, int n, int l, double *lna, double *lnb, sign_t *sign_a, sign_t *sign_b)
-    void casimir_lnab_perf(casimir_t *self, int n, int l, double *lna, double *lnb, sign_t *sign_a, sign_t *sign_b)
+    void casimir_lnab0(int l, double *a0, sign_t *sign_a0, double *b0, sign_t *sign_b0);
+    void casimir_lnab(casimir_t *self, double nT, int l, double *lna, double *lnb, sign_t *sign_a, sign_t *sign_b);
+    void casimir_lnab_perf(casimir_t *self, double nT, int l, double *lna, double *lnb, sign_t *sign_a, sign_t *sign_b);
 
     void casimir_rp(casimir_t *self, double nT, double k, double *r_TE, double *r_TM)
 
-    void casimir_logdetD0(casimir_t *self, int m, double *EE, double *MM)
-    double casimir_logdetD(casimir_t *self, int n, int m)
+    void casimir_logdetD0(casimir_t *self, int m, double *EE, double *MM);
+    double casimir_logdetD(casimir_t *self, double nT, int m);
 
 
 cdef extern from "sfunc.h":
     double lfac(unsigned int n);
     double logi(unsigned int x);
 
-    void Plm_array(int lmax, int m, double x, double factor, double array[]);
-
-    double factorial2(unsigned int n);
     double ln_factorial2(unsigned int n);
 
-    double bessel_lnInu(const int n, const double x);
-    double bessel_lnKnu(const int n, const double x);
     void bessel_lnInuKnu(int nu, const double x, double *lnInu_p, double *lnKnu_p);
 
-    void Plm_array(int lmax, int m, double x, double factor, double array[]);
+    double Plm(int l, int m, double x, double factor, int mode);
     double Plm_estimate(int l, int m, double x);
 
 
@@ -83,10 +77,6 @@ class sfunc:
             log(7!! = 7*5*3*1.
         """
         return ln_factorial2(n)
-
-    def factorial2(unsigned int n):
-        """Calculate double factorial n!!"""
-        return factorial2(n)
 
     def lfac(unsigned n):
         """Calculate log(n!)"""
@@ -113,26 +103,19 @@ class sfunc:
 
     def lnInu(int nu, double x):
         """Modified Bessel function I_{nu+1/2}(x) (see lnInuKnu)"""
-        return bessel_lnInu(nu, x)
+        I,K = sfunc.lnInuKnu(nu,x)
+        return I
 
     def lnKnu(int nu, double x):
         """Modified Bessel function K_{nu+1/2}(x) (see lnInuKnu)"""
-        return bessel_lnKnu(nu, x)
+        I,K = sfunc.lnInuKnu(nu,x)
+        return K
 
     def Plm_estimate(int l, int m, double x):
         return Plm_estimate(l, m, x)
 
-    def Plm(int lmax, int m, double x, double factor=1):
-        cdef int i, elems = lmax-m+1
-        array_py = np.empty(elems)
-        cdef double *array = <double *>malloc(elems*sizeof(double))
-        Plm_array(lmax, m, x, factor, array)
-
-        for i in range(elems):
-            array_py[i] = array[i]
-
-        free(array)
-        return array_py
+    def Plm(int l, int m, double x, double factor=1, int mode=0):
+        return Plm(l,m,x,factor,mode)
 
 
 cdef class Casimir:
@@ -140,50 +123,36 @@ cdef class Casimir:
     cpdef args
     cpdef epsilonm1
 
-    def __init__(self, LbyR, T, verbose=False, debug=False, lmax=None, precision=None, detalg="LU"):
+    def __init__(self, LbyR, verbose=False, debug=False, ldim=None, tolerance=None, detalg="LU"):
         """Initialize Casimir object
 
         Required arguments:
             LbyR: aspect ration L/R, LbyR > 0
-            T:    temperature in units of ħc/(2π*kB*(L+R)), T > 0
 
         Optional arguments:
-            lmax:      truncation of vector space
-            precision  XXX
+            ldim:      truncation of vector space
+            tolerance  relative error for numerical integration
             detalg:    LU, QR, EIG
-            threshold: XXX
+            threshold: threshold for matrix elements
             debug:     flag, print debugging information
             verbose:   flag, print some addition information
         """
         if LbyR <= 0:
-            raise ValueError("LbyR must be positive")
-        if T <= 0:
-            raise ValueError("T must be positive")
-        if lmax != None and lmax <= 0:
-            raise ValueError("lmax must be positive")
-        if precision != None and precision <= 0:
-            raise ValueError("precision must be positive")
-        if detalg != None and detalg not in ("LU", "QR", "EIG"):
-            raise ValueError("detalg must be one of LU, QR or EIG")
+            raise ValueError("invalid value for LbyR")
 
-        self.casimir = casimir_init(LbyR, T)
+        self.casimir = casimir_init(LbyR)
         if self.casimir == NULL:
-            raise RuntimeError("casimir_init returned NULL. Something went terrible wrong.")
+            raise RuntimeError("casimir_init returned NULL.")
 
-        if lmax:
-            casimir_set_lmax(self.casimir, lmax)
-        if precision:
-            casimir_set_precision(self.casimir, precision)
-        if debug:
-            casimir_set_debug(self.casimir, True)
-        if verbose:
-            casimir_set_verbose(self.casimir, verbose)
-        if detalg == "LU":
-            casimir_set_detalg(self.casimir, DETALG_LU)
-        elif detalg == "QR":
-            casimir_set_detalg(self.casimir, DETALG_QR)
-        elif detalg == "EIG":
-            casimir_set_detalg(self.casimir, DETALG_EIG)
+        if ldim != None:
+            self.set_ldim(ldim)
+        if tolerance != None:
+            self.set_tolerance(tolerance)
+        if detalg != None:
+            self.set_detalg(detalg)
+
+        self.set_debug(debug)
+        self.set_verbose(verbose)
 
     def __dealloc__(self):
         casimir_free(self.casimir)
@@ -191,17 +160,43 @@ cdef class Casimir:
     def get_debug(self):
         return casimir_get_debug(self.casimir)
 
+    def set_debug(self, bool debug):
+        return casimir_set_debug(self.casimir, debug)
+
     def get_verbose(self):
         return casimir_get_verbose(self.casimir)
 
-    def get_lmax(self):
-        return casimir_get_lmax(self.casimir)
+    def set_verbose(self, bool debug):
+        return casimir_set_verbose(self.casimir, debug)
+
+    def get_ldim(self):
+        return casimir_get_ldim(self.casimir)
+
+    def set_ldim(self, int ldim):
+        if ldim < 1:
+            raise ValueError("invalid value for ldim")
+        return casimir_set_ldim(self.casimir, ldim)
 
     def get_detalg(self):
         return casimir_get_detalg(self.casimir)
 
-    def get_precision(self):
-        return casimir_get_precision(self.casimir)
+    def set_detalg(self, detalg):
+        if detalg == "LU":
+            casimir_set_detalg(self.casimir, DETALG_LU)
+        elif detalg == "QR":
+            casimir_set_detalg(self.casimir, DETALG_QR)
+        elif detalg == "EIG":
+            casimir_set_detalg(self.casimir, DETALG_EIG)
+        else:
+            raise ValueError("invalid value for detalg")
+
+    def get_tolerance(self):
+        return casimir_get_tolerance(self.casimir)
+
+    def set_tolerance(self, double tolerance):
+        if tolerance <= 0:
+            raise ValueError("invalid value for tolerance")
+        return casimir_set_tolerance(self.casimir, tolerance)
 
     def set_epsilonm1(Casimir self, epsilonm1, args):
         """Set callback function epsilonm1
@@ -221,12 +216,13 @@ cdef class Casimir:
         casimir_set_epsilonm1(self.casimir, &__epsilonm1, ptr)
 
     def __repr__(self):
-        s  = "L/R = %.8g\n" % self.casimir.LbyR
-        s += "T   = %.8g\n" % self.casimir.T
-        s += "lmax      = %d\n" % self.get_lmax()
-        s += "precision = %g\n" % self.get_precision()
+        s  = "L/R       = %.8g\n" % self.casimir.LbyR
+        s += "ldim      = %d\n" % self.get_ldim()
+        s += "tolerance = %g\n" % self.get_tolerance()
         s += "threshold = %g\n" % self.casimir.threshold
-        s += "detalg    = %d" % self.get_detalg()
+        s += "detalg    = %d\n" % self.get_detalg()
+        s += "verbose   = %s\n" % ("True" if self.get_verbose() else "False")
+        s += "debug     = %s"   % ("True" if self.get_debug()   else "False")
         return s
 
 
@@ -241,7 +237,7 @@ cdef class Casimir:
         casimir_lnab0(l, &a0, &sign_a0, &b0, &sign_b0)
         return a0, sign_a0, b0, sign_b0
 
-    def lnab(Casimir self, int n, int l):
+    def lnab(Casimir self, double nT, int l):
         """Calculate Mie coefficients a_l, b_l
 
         Calculate Mie coefficients for order l and argument χ=nT*R/(R+L). The
@@ -249,14 +245,14 @@ cdef class Casimir:
         """
         cdef double lna,lnb
         cdef sign_t sign_a, sign_b
-        casimir_lnab(self.casimir, n, l, &lna, &lnb, &sign_a, &sign_b)
+        casimir_lnab(self.casimir, nT, l, &lna, &lnb, &sign_a, &sign_b);
         return lna, sign_a, lnb, sign_b
 
-    def lnab_perf(Casimir self, int n, int l):
+    def lnab_perf(Casimir self, double nT, int l):
         """Calculate Mie coefficients for perfect reflectors"""
         cdef double lna,lnb
         cdef sign_t sign_a, sign_b
-        casimir_lnab_perf(self.casimir, n, l, &lna, &lnb, &sign_a, &sign_b)
+        casimir_lnab_perf(self.casimir, nT, l, &lna, &lnb, &sign_a, &sign_b)
         return lna, sign_a, lnb, sign_b
 
 
@@ -273,9 +269,9 @@ cdef class Casimir:
         casimir_logdetD0(self.casimir, m, &EE, &MM)
         return EE, MM
 
-    def logdetD(Casimir self, int n, int m):
+    def logdetD(Casimir self, double nT, int m):
         """Calculate determinant of scattering matrix"""
-        return casimir_logdetD(self.casimir, n, m)
+        return casimir_logdetD(self.casimir, nT, m)
 
 
 cdef double __epsilonm1(double xi, void *userdata):
