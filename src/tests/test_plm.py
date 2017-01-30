a6c8bd81a1faa1056a22ec95eefac5fbc65fb6fa
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-01-30 10:38:55 +0100

    Adjust script to create test_Plm.c

--- src/tests/test_plm.py
+++ src/tests/test_plm.py
@@ -3,7 +3,7 @@
 from __future__ import division
 from mpmath import *
 
-mp.dps = 150
+mp.dps = 50
 
 def Plm(l,m,x):
     if m % 2 == 0:
@@ -28,15 +28,16 @@ if __name__ == "__main__":
     print("    unittest_init(&test, \"Plm\", \"Test associated Legendre polynomials\");")
     print()
 
-    for l in (1, 5, 10, 50, 100, 500, 1000, 2000, 5000, 10000, 15000):
+    for l in (1, 5, 10, 50, 100, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000):
         for m in (0,1,5,10,50,100,500,1000):
-            for x in (1.1, 2, 5, 10, 100, 1000, 1e6):
+            for x in (1.01, 1.1, 1.5, 2, 5, 10, 50, 100, 500, 1000, 1e6):
                 if l >= m:
                     try:
                         value = Plm(l,m,mpf(x))
                         print("    AssertAlmostEqual(&test, Plm(%d,%d,%g,1,1), %s);" % (l,m,x,float(log(abs(value)))))
                     except:
                         print("    // AssertAlmostEqual(&test, Plm(%d,%d,%g,1,1), ???);" % (l,m,x))
+        print()
 
     print()
     print("    return test_results(&test, stderr);")
