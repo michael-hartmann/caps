a4dda4d44b38d372ad334a96d2e445f7460a9096
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-02-23 19:02:04 +0100

    add functions to calculate modified Bessel function at integer order

---
+++ src/besselI.h
@@ -0,0 +1,12 @@
+#ifndef __BESSEL_I
+#define __BESSEL_I
+
+double besselI0(double x);
+double besselI0e(double x);
+
+double besselI1(double x);
+double besselI1e(double x);
+
+double besselI(int n, double x);
+
+#endif
