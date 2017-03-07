949de00c76f9e6864bf6cf7afa3840a5cadd51ae
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-03-07 14:01:42 -0300

    simplify plot script

--- data/pfaT0/plot.py
+++ data/pfaT0/plot.py
@@ -3,37 +3,22 @@
 from pyx import *
 from glob import glob
 from math import pi,log,exp
-from numpy import polyfit
 
+def slurp(filenames):
+    data = []
+    for filename in filenames:
+        with open(filename, "r") as f:
+            for line in f:
+                line = line.strip()
+                empty = line == ""
+                comment = line.startswith("#")
+                if not(empty or comment):
+                    # L/R, lmax, order, alpha, F(T=0)
+                    LbyR,lmax,order,alpha,F = map(float, line.split(","))
+                    ratio = F/pfa(LbyR)
+                    data.append((LbyR, F, ratio, ratio/bimonte(LbyR)))
 
-def slurp_alt(filename, data=[]):
-    with open(filename, "r") as f:
-        for line in f:
-            line = line.strip()
-            empty = line == ""
-            comment = line.startswith("#")
-            if not(empty or comment):
-                # L/R, lmax, order, alpha, F(T=0)
-                LbyR,T,F,error = map(float, line.split(","))
-                ratio = F/pfa(LbyR)
-                data.append((LbyR, F, ratio, ratio/bimonte(LbyR)))
-
-    return data
-
-
-def slurp(filename, data=[]):
-    with open(filename, "r") as f:
-        for line in f:
-            line = line.strip()
-            empty = line == ""
-            comment = line.startswith("#")
-            if not(empty or comment):
-                # L/R, lmax, order, alpha, F(T=0)
-                LbyR,lmax,order,alpha,F = map(float, line.split(","))
-                ratio = F/pfa(LbyR)
-                data.append((LbyR, F, ratio, ratio/bimonte(LbyR)))
-
-    return data
+    return sorted(data, key=lambda x: x[0])
 
 
 if __name__ == "__main__":
@@ -50,17 +35,7 @@ if __name__ == "__main__":
     theta1,theta2 = 1./3 - 20/pi**2, -4.52
     bimonte = lambda x: 1+theta1*x+theta2*x**2*log(x)
 
-    # read in data
-    data_eta7 = []
-    for filename in glob("eta7/slurm-*.out"):
-        slurp(filename, data_eta7)
-    data_eta7 = sorted(data_eta7, key=lambda x: x[0])
-
-    data_eta8 = []
-    for filename in glob("eta8/slurm-*.out"):
-        slurp(filename, data_eta8)
-    data_eta8 = sorted(data_eta8, key=lambda x: x[0])
-
+    data = slurp(glob("eta8/slurm-*.out"))
 
     attrs = [color.gradient.RedBlue]
 
@@ -77,13 +52,13 @@ if __name__ == "__main__":
             width = 4,
             xpos = 1.7,
             ypos = 1,
-            x = graph.axis.lin(max=0.008, min=0.001),
+            x = graph.axis.lin(max=0.005, min=0.001),
             y = graph.axis.lin(max=0.997, min=0.9875)
         ))
 
 
         g.plot(
-            graph.data.points(data_eta8, x=1, y=3, title="numerisch"),
+            graph.data.points(data, x=1, y=3, title="numerisch"),
             [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
         )
 
@@ -91,7 +66,7 @@ if __name__ == "__main__":
         g.plot(graph.data.function(f, title="Bimonte et al."))
 
         g2.plot(
-            graph.data.points(data_eta8, x=1, y=3, title="numerisch"),
+            graph.data.points(data, x=1, y=3, title="numerisch"),
             [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
         )
 
@@ -105,19 +80,11 @@ if __name__ == "__main__":
         g = graph.graphxy(
             width = 10,
             key   = graph.key.key(pos="bl"),
-            x     = graph.axis.log(title=r"$x=L/R$", max=0.02, min=0.0007),
+            x     = graph.axis.log(title=r"$x=L/R$", max=0.02, min=0.0005),
             y     = graph.axis.lin(min=0.9998, max=1.0003, title=r"$\frac{\mathcal{F}(T=0)}{\mathcal{F}_\mathrm{PFA}(T=0)}/\left(1+\theta_1 x + \theta_2 x^2 \log x\right)$"),
         )
 
-        g.plot(
-            [
-                #graph.data.points(data_eta7, x=1, y=4, title=r"$\eta=7$"),
-                graph.data.points(data_eta8, x=1, y=4, title=r"$\eta=8$")
-            ],
-            #graph.data.points(data2, x=1, y=4, title=r"numerisch (unterschiedliche $\ell_\mathrm{max}$)"),
-            #graph.data.points(data_alt, x=1, y=4, title=r"numerisch (alt, $\ell_\mathrm{max}=6.2 L/R$)")],
-            [graph.style.symbol(graph.style.symbol.circle, size=0.06, symbolattrs=attrs), graph.style.line()]
-        )
+        g.plot(graph.data.points(data, x=1, y=4, title=r"$\eta=8$"), [graph.style.symbol(graph.style.symbol.circle, size=0.06, symbolattrs=attrs), graph.style.line()])
 
         g.finish()
         g.stroke(g.ygridpath(1), [style.linestyle.dashed])
