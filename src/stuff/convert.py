45bc473ae4e1d01effee6d7b77254ed94bf62b9e
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-03-09 10:06:17 -0300

    add script convert

---
+++ src/stuff/convert.py
@@ -0,0 +1,22 @@
+def eV2scaled(R, LbyR, ωp, γ):
+    ħ = 6.582119514e-16 # reduced Planck constant [eV/s]
+
+    ωp_scaled = ωp/ħ*R*(1+LbyR)
+    γ_scaled  =  γ/ħ*R*(1+LbyR)
+
+    return ωp_scaled, γ_scaled
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) < 5:
+        print("%s R L/R ωp γ" % argv[0])
+        exit(1)
+
+    R    = float(argv[1])
+    LbyR = float(argv[2])
+    ωp   = float(argv[3])
+    γ    = float(argv[4])
+
+    print("%.15e %.15e" % eV2scaled(R, LbyR, ωp, γ))
