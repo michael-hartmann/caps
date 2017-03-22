934e943bc398ac4848315a571da0c04cad2ba0dd
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-03-22 11:56:16 -0300

    add material.c

---
+++ src/material.h
@@ -0,0 +1,19 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+typedef struct {
+    char filename[512];
+    double omegap_low, gamma_low;
+    double omegap_high, gamma_high;
+    double xi_min, xi_max;
+    size_t points;
+    double *xi, *epsm1;
+} material_t;
+
+material_t *material_init(const char *filename);
+void material_info(material_t *material, FILE *stream, const char *prefix);
+void material_free(material_t *material);
+
+double material_epsilonm1(double xi, void *args);
+
+#endif
