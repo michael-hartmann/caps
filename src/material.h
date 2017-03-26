7b4044ed6aace3271b0443f9d56f016d9fbe46c3
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-03-26 22:24:51 +0200

    use only surface-to-surface distance

--- src/material.h
+++ src/material.h
@@ -3,7 +3,7 @@
 
 typedef struct {
     char filename[512];
-    double L,R;
+    double calL;
     double omegap_low, gamma_low;
     double omegap_high, gamma_high;
     double xi_min, xi_max;
@@ -11,7 +11,7 @@ typedef struct {
     double *xi, *epsm1;
 } material_t;
 
-material_t *material_init(const char *filename, double L, double R);
+material_t *material_init(const char *filename, double calL);
 void material_info(material_t *material, FILE *stream, const char *prefix);
 void material_free(material_t *material);
 
