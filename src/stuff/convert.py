7e87005f8cc1047350baea06372cafe9c55cd008
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-03-27 14:32:00 +0200

    use L and R instead of R and L/R

--- src/stuff/convert.py
+++ src/stuff/convert.py
@@ -5,10 +5,10 @@ hbar_eV = 6.582119514e-16 # eV s
 kB      = 1.38064852e-23  # m² kg / ( K s² )
 c       = 299792458       # m/s
 
-def scaled(R, LbyR, T, ωp, γ):
-    T_scaled  = 2*pi*kB*R*(1+LbyR)*T/(hbar*c)
-    ωp_scaled = ωp/(hbar_eV*c)*R*(1+LbyR)
-    γ_scaled  =  γ/(hbar_eV*c)*R*(1+LbyR)
+def scaled(L, R, T, ωp, γ):
+    T_scaled  = 2*pi*kB*(L+R)*T/(hbar*c)
+    ωp_scaled = ωp/(hbar_eV*c)*(L+R)
+    γ_scaled  =  γ/(hbar_eV*c)*(L+R)
 
     return T_scaled, ωp_scaled, γ_scaled
 
@@ -16,20 +16,20 @@ def scaled(R, LbyR, T, ωp, γ):
 if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) < 6:
-        print("%s R L/R T ωp γ" % argv[0])
+    try:
+        L  = float(eval(argv[1]))
+        R  = float(eval(argv[2]))
+        T  = float(eval(argv[3]))
+        ωp = float(eval(argv[4]))
+        γ  = float(eval(argv[5]))
+    except:
+        print("%s L R T ωp γ" % argv[0])
         exit(1)
 
-    R    = eval(argv[1])
-    LbyR = eval(argv[2])
-    T    = eval(argv[3])
-    ωp   = eval(argv[4])
-    γ    = eval(argv[5])
+    T_scaled, ωp_scaled, γ_scaled = scaled(L, R, T, ωp, γ)
 
-    T_scaled, ωp_scaled, γ_scaled = scaled(R, LbyR, T, ωp, γ)
-
-    print("R   = %.15g" % R)
-    print("L/R = %.15g" % LbyR)
-    print("T   = %.15g" % T_scaled)
-    print("ωp  = %.15g" % ωp_scaled)
-    print("γ   = %.15g" % γ_scaled)
+    print("L  = %.15g" % L)
+    print("R  = %.15g" % R)
+    print("T  = %.15g" % T_scaled)
+    print("ωp = %.15g" % ωp_scaled)
+    print("γ  = %.15g" % γ_scaled)
