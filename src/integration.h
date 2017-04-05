3952323d1b02a10932ec819e4233f8b2b87529ad
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-05 13:54:58 +0200

    add methods to compute logdet for plasma and xi=0

--- src/integration.h
+++ src/integration.h
@@ -6,6 +6,11 @@
 #include "libcasimir.h"
 #include "integration.h"
 
+typedef struct {
+    double LbyR, omegap, epsrel;
+    HashTable *cache;
+} integration_plasma_t;
+
 integration_t *casimir_integrate_init(casimir_t *self, double xi, int m, double epsrel);
 void casimir_integrate_free(integration_t *integration);
 
@@ -16,5 +21,9 @@ double casimir_integrate_A(integration_t *self, int l1, int l2, polarization_t p
 double casimir_integrate_B(integration_t *self, int l1, int l2, polarization_t p, sign_t *sign);
 double casimir_integrate_C(integration_t *self, int l1, int l2, polarization_t p, sign_t *sign);
 double casimir_integrate_D(integration_t *self, int l1, int l2, polarization_t p, sign_t *sign);
+
+integration_plasma_t *casimir_integrate_plasma_init(double RbyL, double omegap, double epsrel);
+double casimir_integrate_plasma(integration_plasma_t *self, int l1, int l2, int m);
+void casimir_integrate_plasma_free(integration_plasma_t *self);
 
 #endif
