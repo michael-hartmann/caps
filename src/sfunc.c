bf42ca72b40f8829a369ae0df3dbd8ffa2e8df58
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-05 15:33:32 +0200

    add function sqrtpm1

--- src/sfunc.c
+++ src/sfunc.c
@@ -72,6 +72,29 @@ double kahan_sum(double input[], size_t N)
     return sum;
 }
 
+/** @brief Compute sqrt(1+x)-1
+ *
+ * If x is small, sqrt(1+x)≈1 and a loss of significance occures when
+ * calculating sqrt(1+x)-1.
+ *
+ * For this reason we use the Taylor series
+ *     sqrt(1+x) ≈ 1 + x/2 - x²/8 + x³/16 - 5*x^4/128 + ...
+ * to avoid a loss of significance if x is small.
+ *
+ * @param [in] x
+ * @retval sqrt(1+x)-1
+ */
+double sqrtpm1(double x)
+{
+    if(fabs(x) < 1e-5)
+        /* sqrt(1+x)-1 = x/2 - x²/8 + x³/16 - 5*x^4/128 + O(x^5) */
+        return x/2 - pow_2(x)/8 + pow_3(x)/16 - 5*pow_4(x)/128;
+    else
+        return sqrt(1+x)-1;
+}
+
+
+
 /**
 * @name Add numbers given as logarithms
 */
