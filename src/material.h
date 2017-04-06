75ebc8093666a7ef39560453ac8d8029633b7cc6
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-06 14:53:58 +0200

    add function material_get_extrapolation

--- src/material.h
+++ src/material.h
@@ -15,6 +15,8 @@ material_t *material_init(const char *filename, double calL);
 void material_info(material_t *material, FILE *stream, const char *prefix);
 void material_free(material_t *material);
 
+void material_get_extrapolation(material_t *material, double *omegap_low, double *gamma_low, double *omegap_high, double *gamma_high);
+
 double material_epsilonm1(double xi_scaled, void *args);
 
 #endif
