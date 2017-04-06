a3322b96b98ffa22f960915411a909881266ca94
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-06 10:28:11 +0200

    rename tolerance to epsrel

--- src/libcasimir.h
+++ src/libcasimir.h
@@ -44,7 +44,7 @@ typedef enum { TE, TM } polarization_t;
 /* default values */
 #define CASIMIR_MINIMUM_LDIM 20 /**< minimum value for lmax */
 #define CASIMIR_FACTOR_LDIM 5   /**< by default: lmax=ceil(5/LbyR) */
-#define CASIMIR_TOLERANCE 1e-8  /**< default tolerance for integration */
+#define CASIMIR_EPSREL 1e-8  /**< default relative error for integration */
 
 /**
  * The Casimir object. This structure stores all essential information about
@@ -77,9 +77,9 @@ typedef struct casimir
      * @name accuracy and numerical parameters
      */
      /*@{*/
-    int ldim;           /**< truncation value for vector space \f$\ell_\mathrm{max}\f$ */
-    double tolerance;   /**< tolerance for integration */
-    detalg_t detalg;    /**< algorithm to calculate determinant */
+    int ldim;        /**< truncation value for vector space \f$\ell_\mathrm{max}\f$ */
+    double epsrel;   /**< relative error for integration */
+    detalg_t detalg; /**< algorithm to calculate determinant */
 
     bool verbose; /**< verbose flag */
 
@@ -140,8 +140,8 @@ int casimir_set_ldim(casimir_t *self, int ldim);
 detalg_t casimir_get_detalg(casimir_t *self);
 void casimir_set_detalg(casimir_t *self, detalg_t detalg);
 
-double casimir_get_tolerance(casimir_t *self);
-int    casimir_set_tolerance(casimir_t *self, double tolerance);
+double casimir_get_epsrel(casimir_t *self);
+int    casimir_set_epsrel(casimir_t *self, double epsrel);
 
 void casimir_lnab0(int l, double *a0, sign_t *sign_a0, double *b0, sign_t *sign_b0);
 void casimir_lnab(casimir_t *self, double nT, int l, double *lna, double *lnb, sign_t *sign_a, sign_t *sign_b);
