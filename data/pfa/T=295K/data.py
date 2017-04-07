9bb4d8de37306947679b0dfb9dfa2de6df5af974
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-07 18:21:08 +0200

    add

---
+++ data/pfa/T=295K/data.py
@@ -0,0 +1,94 @@
+import numpy as np
+from glob import glob
+from math import pi
+
+# constants
+kB   = 1.38064852e-23 # m² kg 1/s² 1/K
+hbar = 1.0545718e-34  # m² kg / s
+c    = 299792458      # m/s
+
+R = 151.3e-6
+T = 295
+
+def slurp(filenames):
+    rows = len(filenames)
+    data = np.zeros((rows,7))
+
+    for i,filename in enumerate(filenames):
+        with open(filename, "r") as f:
+            drude = plasma = 0
+            for line in f:
+                line = line.strip()
+                if "# plasma" in line:
+                    _,line = line.split("=", 1)
+
+                    line = line.strip()
+                    plasma = float(line[:line.find(" ")])
+                    continue
+                if "# xi=0" in line:
+                    line = line[16:]
+                    line = line[:line.find(",")]
+                    drude = float(line)
+                    continue
+                if line == "" or line[0] == "#":
+                    continue
+
+                # L/R, L, R, T, ldim, F*(L+R)/(ħc)
+                LbyR, L, R, T, ldim, F = map(float, line.split(","))
+
+                assert(T == 295)
+                T_scaled = 2*pi*kB*(L+R)/(hbar*c)*T
+
+                data[i,0] = LbyR
+                data[i,1] = L
+                data[i,2] = R
+                data[i,3] = T
+                data[i,4] = ldim
+                data[i,5] = F
+                data[i,6] = F + (plasma-drude/2)/2*T_scaled/pi
+
+    return data
+
+def deriv(x,y, deriv=1, accuracy=2):
+    coefficients = {
+        (1,2): (-1/2, 0, 1/2),
+        (1,4): (1/12, -2/3, 0, 2/3, -1/12),
+        (1,6): (-1/60, 3/20, -3/4, 0, 3/4, -3/20, 1/60),
+        (1,8): (1/280, -4/105, 1/5, -4/5, 0, 4/5, -1/5, 4/105, -1/280),
+
+        (2,2): (1, -2, 1),
+        (2,4): (-1/12, 4/3, -5/2, 4/3, -1/12),
+        (2,6): (1/90, -3/20, 3/2, -49/18, 3/2, -3/20, 1/90),
+        (2,8): (-1/560, 8/315, -1/5, 8/5, -205/72, 8/5, -1/5, 8/315, -1/560)
+    }
+
+    npts = len(x)
+    h = (x[-1]-x[0])/(npts-1)
+
+    c = coefficients[(deriv, accuracy)]
+    p = len(c)//2
+
+    dy = np.zeros(npts-2*p)
+    for i in range(npts-2*p):
+        for j in range(len(c)):
+            dy[i] += c[j]*y[i+j]
+
+    dy /= h**deriv
+    return np.copy(x[p:-p]), dy
+
+
+if __name__ == "__main__":
+    data = slurp(sorted(glob("gold/gold*.out")))
+    L = data[:,1]
+    E_drude  = data[:,5]/(L+R)*(hbar*c)
+    E_plasma = data[:,6]/(L+R)*(hbar*c)
+
+    dx, dF_drude  = deriv(L, E_drude,  deriv=2, accuracy=4)
+    dx, dF_plasma = deriv(L, E_plasma, deriv=2, accuracy=4)
+    dx *= 1e9
+    P_drude = 1/(2*pi*R)*dF_drude*1000
+    P_plasma = 1/(2*pi*R)*dF_plasma*1000
+
+    print("# L (nm), P (Drude, mPa), P (Plasma, mPa)")
+    for i,x in enumerate(dx):
+        print("%.15g, %.15g, %.15g" % (x, P_drude[i], P_plasma[i]))
---
+++ data/pfa/T=295K/pfa.py
@@ -0,0 +1,112 @@
+import numpy as np
+from math import exp, fsum, pi, sqrt
+from scipy.integrate import quad
+from scipy import interpolate
+
+# constants
+c    = 299792458       # speed of light [m/s]
+kB   = 1.38064852e-23  # Boltzmann constant [m² kg / (s² K)]
+hbar = 1.0545718e-34   # hbar [J s]
+hbar_eV = 6.582119514e-16 # hbar [eV s]
+
+class PFA:
+    def __init__(self, R, T, epsilonm1, omegap=0, args=None):
+        """R in m, T in K, omegap and gamma in eV"""
+        self.omegap = omegap
+        self.R = R
+        self.T = T
+        self.epsilonm1 = epsilonm1
+        self.args = args
+
+
+    def rp(self, xi, kappa):
+        if xi == 0:
+            # plasma    
+            if self.omegap:
+                beta = sqrt(1+(self.omegap/(c*kappa))**2)
+                rTE = (1-beta)/(1+beta)
+                return rTE,1
+            else:
+                # Drude
+                return 0,1
+
+        epsm1 = self.epsilonm1(xi, self.args)
+        beta = sqrt(1+ (xi/(c*kappa))**2*epsm1 )
+
+        rTE = (1-beta)/(1+beta)
+        rTM = (epsm1+1-beta)/(epsm1+1+beta)
+
+        return rTE, rTM
+
+
+    def P(self, L):
+        def f(t,xi):
+            """Integrand \sum_p t² rp² e^(-2t)/(1-rp² e^(-2t))"""
+            kappa = t/L
+            rTE, rTM = self.rp(xi,kappa)
+            rTE2, rTM2 = rTE**2, rTM**2
+            exp_f = exp(-2*t)
+            return t**2*exp_f*( rTE2/(1-rTE2*exp_f) + rTM2/(1-rTM2*exp_f) )
+
+        n = 0
+        terms = []
+        while True:
+            xi_n = 2*pi*n*kB*self.T/hbar
+            I,err = quad(f, xi_n*L/c, np.inf, args=(xi_n,), epsabs=0, epsrel=1e-10)
+            terms.append(I)
+
+            if abs(I/terms[0]) < 1e-8:
+                break
+
+            n += 1
+
+        terms[0] /= 2
+        return -(kB*self.T)/(2*pi)/L**3*2*fsum(terms)
+
+
+def get_epsilonm1(filename, omega_p, gamma):
+    data = np.loadtxt(filename)
+
+    data_xi  = data[:,0]
+    data_eps = data[:,1]
+    xi_min, xi_max = data_xi[0], data_xi[-1]
+
+    f = interpolate.interp1d(data_xi, data_eps, kind="linear")
+
+    def epsilonm1(xi,args):
+        if xi < xi_min:
+            return omega_p**2/(xi*(xi+gamma))
+     
+        if xi > xi_max:
+            # XXX
+            return 0
+
+        return f(xi)-1
+
+    return epsilonm1
+
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    R = 151.3e-6
+    T = 295
+
+    omegap = 9/hbar_eV    # plasma frequency 9eV
+    gamma  = 0.03/hbar_eV # dissipation 0.03
+    filename = "../../materials/GoldEpsIm.dat"
+
+    start = float(argv[1])
+    stop  = float(argv[2])
+    N     = int(argv[3])
+
+    print("# L (nm), P (mPa, Drude), P (mPa, Plasma)")
+    for Li in np.linspace(start, stop, N):
+        pfa = PFA(R,T,get_epsilonm1(filename, omegap, gamma))
+        P_drude = pfa.P(Li*1e-9)*1000 # in mPa
+
+        pfa = PFA(R,T,get_epsilonm1(filename, omegap, gamma), omegap=omegap)
+        P_plasma = pfa.P(Li*1e-9)*1000 # in mPa
+
+        print("%.15g, %.12g, %.12g" % (Li,P_drude, P_plasma))
