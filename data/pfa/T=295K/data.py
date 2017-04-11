713d50f44f7a5f87fd7b20e99527288c527254a3
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-11 15:13:08 +0200

    simplify script

--- data/pfa/T=295K/data.py
+++ data/pfa/T=295K/data.py
@@ -7,12 +7,8 @@ kB   = 1.38064852e-23 # m² kg 1/s² 1/K
 hbar = 1.0545718e-34  # m² kg / s
 c    = 299792458      # m/s
 
-R = 151.3e-6
-T = 295
-
 def slurp(filenames):
-    rows = len(filenames)
-    data = np.zeros((rows,7))
+    data = []
 
     for i,filename in enumerate(filenames):
         with open(filename, "r") as f:
@@ -34,20 +30,14 @@ def slurp(filenames):
                     continue
 
                 # L/R, L, R, T, ldim, F*(L+R)/(ħc)
-                LbyR, L, R, T, ldim, F = map(float, line.split(","))
-
-                assert(T == 295)
+                LbyR, L, R, T, ldim, F_drude = map(float, line.split(","))
                 T_scaled = 2*pi*kB*(L+R)/(hbar*c)*T
+                F_plasma = F_drude + (plasma-drude)/2*T_scaled/pi
 
-                data[i,0] = LbyR
-                data[i,1] = L
-                data[i,2] = R
-                data[i,3] = T
-                data[i,4] = ldim
-                data[i,5] = F
-                data[i,6] = F + (plasma-drude)/2*T_scaled/pi
+                data.append((L,R,T,ldim,F_drude,F_plasma))
 
-    return data
+    return np.array(sorted(data))
+
 
 def deriv(x,y, deriv=1, accuracy=2):
     coefficients = {
@@ -77,18 +67,28 @@ def deriv(x,y, deriv=1, accuracy=2):
     return np.copy(x[p:-p]), dy
 
 
-if __name__ == "__main__":
-    data = slurp(sorted(glob("gold/gold*.out")))
-    L = data[:,1]
-    E_drude  = data[:,5]/(L+R)*(hbar*c)
-    E_plasma = data[:,6]/(L+R)*(hbar*c)
 
-    dx, dF_drude  = deriv(L, E_drude,  deriv=2, accuracy=4)
-    dx, dF_plasma = deriv(L, E_plasma, deriv=2, accuracy=4)
+if __name__ == "__main__":
+    R = 151.3e-6
+
+    d = {}
+    pfa = np.loadtxt("data_pfa", delimiter=",")
+    for i,L in enumerate(pfa[:,0]):
+        d[L] = (pfa[i,1], pfa[i,2])
+
+    data = slurp(sorted(glob("gold_eta8/slurm*.out"), reverse=True))
+    L = data[:,0]
+    E_drude  = data[:,4]/(L+R)*(hbar*c)
+    E_plasma = data[:,5]/(L+R)*(hbar*c)
+
+    dx, dF_drude  = deriv(L, E_drude,  deriv=2, accuracy=2)
+    dx, dF_plasma = deriv(L, E_plasma, deriv=2, accuracy=2)
+
     dx *= 1e9
-    P_drude = 1/(2*pi*R)*dF_drude*1000
+    P_drude  = 1/(2*pi*R)*dF_drude *1000
     P_plasma = 1/(2*pi*R)*dF_plasma*1000
 
-    print("# L (nm), P (Drude, mPa), P (Plasma, mPa)")
+    print("# L (nm), P (Drude, mPa), P (Plasma, mPa), P (Drude, PFA), P (Plasma, PFA), ratio (Drude), ratio (Plasma)")
     for i,x in enumerate(dx):
-        print("%.15g, %.15g, %.15g" % (x, P_drude[i], P_plasma[i]))
+        index = int(round(x,8))
+        print("%.15g, %.15g, %.15g, %.15g, %.15g, %.15g, %.15g" % (x, P_drude[i], P_plasma[i], d[index][0], d[index][1], P_drude[i]/d[index][0], P_plasma[i]/d[index][1]))
