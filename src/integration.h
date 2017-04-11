93e39c56dfd8cbdb8520952da4443c8c07e4c581
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-11 08:40:51 +0200

    increase speed of Plasma kernel for xi=0
    Introduce a new cache for the ratio of the Bessel functions to increase
    performance.

--- src/integration.h
+++ src/integration.h
@@ -17,8 +17,8 @@ double casimir_integrate_B(integration_t *self, int l1, int l2, polarization_t p
 double casimir_integrate_C(integration_t *self, int l1, int l2, polarization_t p, sign_t *sign);
 double casimir_integrate_D(integration_t *self, int l1, int l2, polarization_t p, sign_t *sign);
 
-integration_plasma_t *casimir_integrate_plasma_init(double omegap, double epsrel);
-double casimir_integrate_plasma(integration_plasma_t *self, int l1, int l2, int m);
+integration_plasma_t *casimir_integrate_plasma_init(casimir_t *casimir, double omegap, double epsrel);
+double casimir_integrate_plasma(integration_plasma_t *self, int l1, int l2, int m, double *ratio1, double *ratio2);
 void casimir_integrate_plasma_free(integration_plasma_t *self);
 
 #endif
