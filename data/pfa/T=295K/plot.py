3378fa6208bca26ebe3023ec3638bf829c664f84
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-15 23:04:04 +0200

    update

--- data/pfa/T=295K/plot.py
+++ data/pfa/T=295K/plot.py
@@ -1,37 +1,60 @@
 import numpy as np
 from pyx import *
 
-data = np.loadtxt("out", delimiter=",")
+data_R10 = np.loadtxt("R=10e-6/data.csv", delimiter=",")
+data_R30 = np.loadtxt("R=30e-6/data.csv", delimiter=",")
+data_R50 = np.loadtxt("R=50e-6/data.csv", delimiter=",")
+data_R151 = np.loadtxt("R=151.3e-6/data.csv", delimiter=",")
 
-text.set(text.LatexRunner)
+x_R10        = data_R10[:,0]
+y_R10_drude  = data_R10[0,1]*(1-data_R10[:,3]/data_R10[:,5])
+y_R10_plasma = data_R10[0,1]*(1-data_R10[:,4]/data_R10[:,6])
 
-L = data[:,0]
-P_drude      = data[:,1]
-P_plasma     = data[:,2]
-P_drude_pfa  = data[:,3]
-P_plasma_pfa = data[:,4]
-ratio_drude  = data[:,5]
-ratio_plasma = data[:,6]
+x_R30        = data_R30[:,0]
+y_R30_drude  = data_R30[0,1]*(1-data_R30[:,3]/data_R30[:,5])
+y_R30_plasma = data_R30[0,1]*(1-data_R30[:,4]/data_R30[:,6])
+
+x_R50        = data_R50[:,0]
+y_R50_drude  = data_R50[0,1]*(1-data_R50[:,3]/data_R50[:,5])
+y_R50_plasma = data_R50[0,1]*(1-data_R50[:,4]/data_R50[:,6])
+
+x_R151        = data_R151[:,0]
+y_R151_drude  = data_R151[0,1]*(1-data_R151[:,3]/data_R151[:,5])
+y_R151_plasma = data_R151[0,1]*(1-data_R151[:,4]/data_R151[:,6])
 
 attrs = [color.gradient.RedBlue]
 
+text.set(text.LatexRunner)
 
-x = L/151300
-y_drude  = 1-ratio_drude
-y_plasma = 1-ratio_plasma
-
-g = graph.graphxy(
+g_drude = graph.graphxy(
     width = 8,
-    key = graph.key.key(pos="tl"),
-    x = graph.axis.lin(title=r"$d/R$"),
-    y = graph.axis.lin(title=r"$1-F^\prime/F^\prime_\mathrm{PFA}$")
+    key = graph.key.key(pos="tr"),
+    x = graph.axis.lin(title=r"$d$ (nm)", divisor=1e-9),
+    y = graph.axis.lin(title=r"$\frac{R}{d} \left(1-F^\prime/F^\prime_\mathrm{PFA}\right)$")
 )
 
-g.plot([
-    graph.data.values(x=x, y=y_drude, title="Drude"),
-    graph.data.values(x=x, y=y_plasma, title="Plasma")
-    ], [graph.style.symbol(graph.style.symbol.changecircle, symbolattrs=attrs, size=0.02)])
+g_drude.plot([
+    graph.data.values(x=x_R10,  y=y_R10_drude/x_R10,  title=r"$R=10\mu\mathrm{m}$"),
+    graph.data.values(x=x_R30,  y=y_R30_drude/x_R30,  title=r"$R=30\mu\mathrm{m}$"),
+    graph.data.values(x=x_R50,  y=y_R50_drude/x_R50,  title=r"$R=50\mu\mathrm{m}$"),
+    graph.data.values(x=x_R151, y=y_R151_drude/x_R151, title=r"$R=151.3\mu\mathrm{m}$"),
+    ], [graph.style.symbol(graph.style.symbol.changecircle, symbolattrs=attrs, size=0.03)])
 
-g.plot(graph.data.function("y(x)=0.4*x"))
+g_drude.writePDFfile("drude")
 
-g.writePDFfile()
+
+g_plasma = graph.graphxy(
+    width = 8,
+    key = graph.key.key(pos="tl"),
+    x = graph.axis.lin(title=r"$d$ (nm)", divisor=1e-9),
+    y = graph.axis.lin(title=r"$\frac{R}{d} \left(1-F^\prime/F^\prime_\mathrm{PFA}\right)$")
+)
+
+g_plasma.plot([
+    graph.data.values(x=x_R10,  y=y_R10_plasma/x_R10,  title=r"$R=10\mu\mathrm{m}$"),
+    graph.data.values(x=x_R30,  y=y_R30_plasma/x_R30,  title=r"$R=30\mu\mathrm{m}$"),
+    graph.data.values(x=x_R50,  y=y_R50_plasma/x_R50,  title=r"$R=50\mu\mathrm{m}$"),
+    graph.data.values(x=x_R151, y=y_R151_plasma/x_R151, title=r"$R=151.3\mu\mathrm{m}$"),
+    ], [graph.style.symbol(graph.style.symbol.changecircle, symbolattrs=attrs, size=0.03)])
+
+g_plasma.writePDFfile("plasma")
