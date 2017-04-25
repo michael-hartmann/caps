f56e966d0b67c78a3ba477d733691bf0374ee628
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-04-25 19:35:39 +0200

    add data.py files

---
+++ data/pfa/T=295K/R10mu/data.py
@@ -0,0 +1,27 @@
+from glob import glob
+from sys import path
+path.append("..")
+from casimir import print_force, print_gradient
+
+def stepsize_force(L):
+    if L > 400e-9:
+        return 3
+    if L > 200e-9:
+        return 2
+    return 1
+
+def stepsize_gradient(L):
+    if L > 400e-9:
+        return 3
+    if L > 200e-9:
+        return 2
+    return 1
+
+if __name__ == "__main__":
+    files = glob("gold_eta10/slurm-*.out")
+
+    f_force    = open("force_10000.csv", "w")
+    print_force(files, stepsize_force, f=f_force)
+
+    f_gradient = open("gradient_10000.csv", "w")
+    print_gradient(files, stepsize_gradient, f=f_gradient)
---
+++ data/pfa/T=295K/R20mu/data.py
@@ -0,0 +1,27 @@
+from glob import glob
+from sys import path
+path.append("..")
+from casimir import print_force, print_gradient
+
+def stepsize_force(L):
+    if L > 300e-9:
+        return 2
+    return 1
+
+def stepsize_gradient(L):
+    if L > 550e-9:
+        return 3
+    if L > 450e-9:
+        return 3
+    if L > 300e-9:
+        return 2
+    return 1
+
+if __name__ == "__main__":
+    files = glob("gold_eta9/slurm-*.out")
+
+    f_force    = open("force_20000.csv", "w")
+    print_force(files, stepsize_force, f=f_force)
+
+    f_gradient = open("gradient_20000.csv", "w")
+    print_gradient(files, stepsize_gradient, f=f_gradient)
