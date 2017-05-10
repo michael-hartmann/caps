52f6812d772a9e52d4ff2f9b53c6078b3f8235b0
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-05-10 16:20:12 +0200

    add option for arbitrary materials

--- src/casimir_logdetD.c
+++ src/casimir_logdetD.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "material.h"
 #include "libcasimir.h"
 #include "sfunc.h"
 #include "utils.h"
@@ -21,13 +22,17 @@ static void usage(FILE *stream)
 "geometry for given n,m,T,L/R.\n"
 "\n"
 "Mandatory options:\n"
-"    -x, --LbyR  L/R\n"
-"    --nT        imaginary frequency ξ in units of c/(L+R)\n"
-"    -m          value of m\n"
+"    -L     separation L\n"
+"    -R     radius R\n"
+"    --nT   imaginary frequency ξ in units of c/(L+R)\n"
+"    -m     value of m\n"
 "\n"
 "Further options:\n"
-"    -L, --ldim LDIM\n"
+"    -l, --ldim LDIM\n"
 "        Set ldim to LDIM. When -L is used, -l will be ignored.\n"
+"\n"
+"    -f, --material FILENAME\n"
+"        Use material described by FILENAME.\n"
 "\n"
 "    -b, --buffering\n"
 "        Enable buffering. By default buffering for stderr and stdout is\n"
@@ -46,8 +51,10 @@ int main(int argc, char *argv[])
 {
     double start_time = now();
 
+    char filename[512] = { 0 };
+
     /* geometry, Matsubara frequency */
-    double LbyR = -1, nT = -1;
+    double L = 0, R = 0, nT = -1;
     int m = -1;
 
     /* numerical parameters */
@@ -63,17 +70,16 @@ int main(int argc, char *argv[])
             { "buffering", no_argument,       0, 'b' },
             { "dense",     no_argument,       0, 'd' },
 
-            { "LbyR",      required_argument, 0, 'x' },
+            { "material",  required_argument, 0, 'f' },
             { "nT",        required_argument, 0, 'T' },
-            { "ldim",      required_argument, 0, 'L' },
-            { "lscale",    required_argument, 0, 'l' },
+            { "ldim",      required_argument, 0, 'l' },
 
             { 0, 0, 0, 0 }
         };
 
         /* getopt_long stores the option index here. */
         int option_index = 0;
-        int c = getopt_long (argc, argv, "x:T:m:l:L:t:bdh", long_options, &option_index);
+        int c = getopt_long (argc, argv, "L:R:T:m:l:f:t:bdh", long_options, &option_index);
 
         /* Detect the end of the options. */
         if(c == -1)
@@ -85,17 +91,23 @@ int main(int argc, char *argv[])
                 /* If this option set a flag, do nothing else now. */
                 if(long_options[option_index].flag != 0)
                     break;
-            case 'x':
-                LbyR = atof(optarg);
+            case 'L':
+                L = atof(optarg);
+                break;
+            case 'R':
+                R = atof(optarg);
                 break;
             case 'T':
                 nT = atof(optarg);
                 break;
-            case 'L':
+            case 'l':
                 ldim = atoi(optarg);
                 break;
             case 'm':
                 m = atoi(optarg);
+                break;
+            case 'f':
+                strncpy(filename, optarg, sizeof(filename)-sizeof(char));
                 break;
             case 'b':
                 buffering = true;
@@ -127,8 +139,10 @@ int main(int argc, char *argv[])
 
     /* check parameters */
     do {
-        if(LbyR <= 0)
-            fprintf(stderr, "-x must be positive.");
+        if(L <= 0)
+            fprintf(stderr, "-L must be positive.");
+        if(R <= 0)
+            fprintf(stderr, "-R must be positive.");
         else if(nT < 0)
             fprintf(stderr, "--nT must be non-negative value.");
         else if(m < 0)
@@ -150,7 +164,21 @@ int main(int argc, char *argv[])
     printf("\n");
 
     casimir_t *casimir;
-    casimir = casimir_init(LbyR);
+    casimir = casimir_init(L/R);
+
+    material_t *material = NULL;
+    if(strlen(filename) > 0)
+    {
+        material = material_init(filename, L+R);
+        if(material == NULL)
+        {
+            fprintf(stderr, "Can't read %s or invalid format\n", filename);
+            usage(stderr);
+            exit(1);
+        }
+
+        casimir_set_epsilonm1(casimir, material_epsilonm1, material);
+    }
 
     /* set dimension of vector space */
     if(ldim)
@@ -166,19 +194,22 @@ int main(int argc, char *argv[])
     {
         double logdet = casimir_logdetD(casimir, nT, m);
 
-        printf("# L/R, ξ*(L+R)/c, m, logdet(Id-M), ldim, time\n");
-        printf("%g, %g, %d, %.16g, %d, %g\n", LbyR, nT, m, logdet, casimir_get_ldim(casimir), now()-start_time);
+        printf("# L, R, ξ*(L+R)/c, m, logdet(Id-M), ldim, time\n");
+        printf("%g, %g, %g, %d, %.16g, %d, %g\n", L, R, nT, m, logdet, casimir_get_ldim(casimir), now()-start_time);
     }
     else /* nT == 0 */
     {
         double logdet_EE, logdet_MM;
         casimir_logdetD0(casimir, m, 0, &logdet_EE, &logdet_MM, NULL);
 
-        printf("# L/R, ξ*(L+R)/c, m, logdet(Id-EE), logdet(Id-MM), lmax, time\n");
-        printf("%g, 0, %d, %.16g, %.16g, %d, %g\n", LbyR, m, logdet_EE, logdet_MM, casimir_get_ldim(casimir), now()-start_time);
+        printf("# L, R, ξ*(L+R)/c, m, logdet(Id-EE), logdet(Id-MM), lmax, time\n");
+        printf("%g, %g, 0, %d, %.16g, %.16g, %d, %g\n", L, R, m, logdet_EE, logdet_MM, casimir_get_ldim(casimir), now()-start_time);
     }
 
     casimir_free(casimir);
+
+    if(material != NULL)
+        material_free(material);
 
     return 0;
 }
