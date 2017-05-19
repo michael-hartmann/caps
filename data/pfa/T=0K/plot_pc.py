99cb9e24c2c774cf751bd17fb5636254ae0aee9a
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-05-19 13:46:05 +0200

    update plot script

--- data/pfa/T=0K/plot_pc.py
+++ data/pfa/T=0K/plot_pc.py
@@ -3,7 +3,16 @@
 from pyx import *
 import numpy as np
 from glob import glob
-from math import pi,log,exp
+
+from scipy.optimize import curve_fit
+
+# formula Bimonte: Bimonte, Emig, Jaffe, Kardar, Casimir forces beyond the proximity approximation
+theta1 = 1/3 - 20/np.pi**2
+
+# PFA formula
+def pfa(x):
+    return -np.pi**3/720.*(x+1)/x**2
+
 
 def slurp(filenames):
     data = []
@@ -23,79 +32,50 @@ def slurp(filenames):
                         LbyR,L,R,T,ldim,F = map(float, line.split(","))
 
                     ratio = F/pfa(LbyR)
-                    data.append((LbyR, F, ratio, ratio/bimonte(LbyR)))
+                    data.append((LbyR, F, ratio, ratio-1, ratio-1-theta1*LbyR))
 
     return np.array(sorted(data, key=lambda x: x[0]))
 
 
-if __name__ == "__main__":
-    plotPFA     = True
-    plotBimonte = False
 
+if __name__ == "__main__":
     # use LaTeX for Pyx
     text.set(text.LatexRunner)
-
-    # PFA formula
-    pfa = lambda x: -pi**3/720.*(x+1)/x**2
-
-    # formula Bimonte: Bimonte, Emig, Jaffe, Kardar, Casimir forces beyond the proximity approximation
-    theta1,theta2 = 1./3 - 20/pi**2, -4.52
-    bimonte = lambda x: 1+theta1*x+theta2*x**2*log(x)
 
     data = slurp(glob("pc_gk/slurm-*.out"))
 
     attrs = [color.gradient.RedBlue]
 
-    if plotPFA:
-        # plot F/F_PFA and Bimonte
-        x = data[:,0]
-        y = (1-data[:,2])/x
-        g = graph.graphxy(
-            width = 10,
-            x = graph.axis.log(title=r"$x=L/R$", min=4e-4),
-            y = graph.axis.lin(title=r"$(R/L)(1-\mathcal{F}/\mathcal{F}_\mathrm{PFA}(T=0))$"),
-            key=graph.key.key(pos="tr", dist=0.1)
-        )
+    x,y = data[:,0], data[:,4]
 
-        #g2 = g.insert(graph.graphxy(
-        #    width = 4,
-        #    xpos = 1.7,
-        #    ypos = 1,
-        #    x = graph.axis.lin(max=0.0045, min=0.00045),
-        #    y = graph.axis.lin(max=0.9998, min=0.9925)
-        #))
+    g = graph.graphxy(
+        width = 10,
+        key   = graph.key.key(pos="tl", dist=0.1),
+        x     = graph.axis.log(title=r"$x=L/R$", min=3e-4),
+        y     = graph.axis.log(title=r"$1-\mathcal{F}/\mathcal{F}_\mathrm{PFA}-\theta_1 R/L$")
+    )
 
+    g.plot(
+        graph.data.values(x=x, y=y, title=None),
+        [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
+    )
 
-        g.plot(
-            graph.data.values(x=x, y=y, title="numerisch"),
-            [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
-        )
-
-        #f = "y(x)=1+%.15g*x+%.15g*x**2*log(x)" % (theta1,theta2)
-        #g.plot(graph.data.function(f, title="Bimonte et al."))
-
-        #g2.plot(
-        #    graph.data.points(data, x=1, y=3, title="numerisch"),
-        #    [graph.style.symbol(graph.style.symbol.circle, size=0.04, symbolattrs=attrs)]
-        #)
-
-        #g2.plot(graph.data.function(f))
-
-        g.writePDFfile("pfaT0.pdf")
+    fit_left, fit_right = 2e-3, 8e-3
+    fitx, fity = [], []
+    for i,LbyR in enumerate(x):
+        if fit_left <= LbyR <= fit_right:
+            fitx.append(x[i])
+            fity.append(y[i])
 
 
-    if plotBimonte:
-        # Plot (F/F_PFA) / Bimonte
-        g = graph.graphxy(
-            width = 10,
-            key   = graph.key.key(pos="bl"),
-            x     = graph.axis.log(title=r"$x=L/R$", max=0.02, min=0.00045),
-            y     = graph.axis.lin(min=0.9998, max=1.0003, title=r"$\frac{\mathcal{F}(T=0)}{\mathcal{F}_\mathrm{PFA}(T=0)}/\left(1+\theta_1 x + \theta_2 x^2 \log x\right)$"),
-        )
+    fitfunc = lambda x, theta2, theta3: theta2*x**1.5 + theta3*x**2
+    params,pcov = curve_fit(fitfunc,fitx,fity,p0=(2.57,-3))
+    theta2,theta3 = params
+    cmd = "y(x) = %.10g*x**1.5 %+.10g*x**2" % (theta2,theta3)
+    g.plot(graph.data.function(cmd, title=r"$\theta_2 x^{1.5} + \theta_3 x^2$, $\theta_2=%.3f$, $\theta_3=%.3f$" % (theta2, theta3)))
 
-        g.plot(graph.data.points(data, x=1, y=4, title=r"$\eta=8$"), [graph.style.symbol(graph.style.symbol.circle, size=0.06, symbolattrs=attrs), graph.style.line()])
+    g.finish()
+    g.stroke(g.xgridpath(fit_left), [style.linestyle.dotted])
+    g.stroke(g.xgridpath(fit_right), [style.linestyle.dotted])
 
-        g.finish()
-        g.stroke(g.ygridpath(1), [style.linestyle.dashed])
-
-        g.writePDFfile("ratio.pdf")
+    g.writePDFfile()
