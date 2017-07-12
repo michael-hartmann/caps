cd68a59b8423a1bf60b60caad418b22d13173eb3
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-12 12:38:35 +0200

    remove

--- data/lowT_fit/data.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from __future__ import division
-import numpy as np
-
-for i,LbyR in enumerate(np.linspace(5,20,200)):
-    Q = 1/(1+LbyR)
-    print "casimir -Q %.15g -T 0.05,0.25,20 -p 1e-10 -L 10 > slurm_addita_%05d.out" % (Q,i)
