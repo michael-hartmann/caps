66495abcf14f26b70123b2fec674fb0a5223e355
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-12 12:22:35 +0200

    update

--- data/pfa/T=0K/plot_pc.py
+++ data/pfa/T=0K/plot_pc.py
@@ -18,11 +18,18 @@ def slurp(filenames):
     data = []
     for filename in filenames:
         with open(filename, "r") as f:
+            absrel = float("nan")
+
             for line in f:
                 line = line.strip()
                 empty = line == ""
                 comment = line.startswith("#")
-                if not(empty or comment):
+                if comment:
+                    index = line.find("absrel=")
+                    if index > 0:
+                        line = line[index+7:]
+                        absrel = float(line)
+                elif not empty:
                     # support old and new format
                     try:
                         # L/R, lmax, order, alpha, F(T=0)
@@ -31,8 +38,10 @@ def slurp(filenames):
                         # L/R, L, R, T, ldim, F*(L+R)/(ħc)
                         LbyR,L,R,T,ldim,F = map(float, line.split(","))
 
-                    ratio = F/pfa(LbyR)
-                    data.append((LbyR, F, ratio, ratio-1, ratio-1-theta1*LbyR))
+                    ratio      = F/pfa(LbyR)
+                    ratio_up   = ratio*(1+absrel)
+                    ratio_down = ratio*(1-absrel)
+                    data.append((LbyR, F, ratio, ratio-1, ratio-1-theta1*LbyR, ratio_up-1-theta1*LbyR, ratio_down-1-theta1*LbyR))
 
     return np.array(sorted(data, key=lambda x: x[0]))
 
@@ -42,11 +51,11 @@ if __name__ == "__main__":
     # use LaTeX for Pyx
     text.set(text.LatexRunner)
 
-    data_eta8  = slurp(glob("pc_gk/eta8/slurm-*.out"))
-    data_eta10 = slurp(glob("pc_gk/eta10/slurm-*.out"))
+    data_eta8  = slurp(glob("pc/eta8/slurm-*.out"))
+    data_eta10 = slurp(glob("pc/eta10/slurm-*.out"))
 
-    attrs = [color.gradient.RedBlue]
-
+    for entry in data_eta10:
+        print(entry[0], -100*entry[3])
 
     g = graph.graphxy(
         width = 10,
@@ -56,11 +65,13 @@ if __name__ == "__main__":
     )
 
     g.plot(
-        [
-        graph.data.points(data_eta8,  x=1, y=5, title=r"$\eta=8$"),
-        graph.data.points(data_eta10, x=1, y=5, title=r"$\eta=10$")
-        ],
-        [graph.style.symbol(graph.style.symbol.changecircle, size=0.04, symbolattrs=attrs)]
+        graph.data.points(data_eta8,  x=1, y=5, ymax=6, ymin=7, title=r"$\eta=8$"),
+        [graph.style.symbol(graph.style.symbol.circle, size=0.05, symbolattrs=[color.cmyk.CadetBlue]), graph.style.errorbar()]
+    )
+
+    g.plot(
+        graph.data.points(data_eta10, x=1, y=5, ymax=6, ymin=7, title=r"$\eta=10$"),
+        [graph.style.symbol(graph.style.symbol.diamond, size=0.05, symbolattrs=[color.cmyk.BrickRed]), graph.style.errorbar()]
     )
 
     x,y = data_eta8[:,0], data_eta8[:,4]
@@ -73,11 +84,11 @@ if __name__ == "__main__":
             fity.append(y[i])
 
 
-    fitfunc = lambda x, theta2, theta3: theta2*x**1.5 + theta3*x**2
-    params,pcov = curve_fit(fitfunc,fitx,fity,p0=(2.57,-3))
-    theta2,theta3 = params
-    cmd = "y(x) = %.10g*x**1.5 %+.10g*x**2" % (theta2,theta3)
-    g.plot(graph.data.function(cmd, title=r"$\theta_2 x^{1.5} + \theta_3 x^2$, $\theta_2=%.3f$, $\theta_3=%.3f$" % (theta2, theta3)))
+    fitfunc = lambda x, theta2: theta2*x**1.5
+    params,pcov = curve_fit(fitfunc,fitx,fity,p0=(2.57,))
+    theta2 = params
+    cmd = "y(x) = %.10g*x**1.5" % (theta2)
+    g.plot(graph.data.function(cmd, title=r"$\theta_2 x^{1.5}$, $\theta_2=%.3f$" % theta2))
 
     g.finish()
     g.stroke(g.xgridpath(fit_left), [style.linestyle.dotted])
