79dc5faa96a01609989804344659f4bc0fc29293
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-18 10:28:46 +0200

    update

--- data/materials/plot.py
+++ data/materials/plot.py
@@ -15,20 +15,24 @@ x2min, x2max = xmin*hbar, xmax*hbar
 g = graph.graphxy(
     width = 8,
     key   = graph.key.key(),
-    x     = graph.axis.log(title=r"$\omega$ (rad/s)", min=xmin, max=xmax),
-    x2    = graph.axis.log(title=r"$\omega$ (eV)",    min=x2min, max=x2max),
+    x2    = graph.axis.log(title=r"$\omega$ (rad/s)", min=xmin, max=xmax),
+    x     = graph.axis.log(title=r"$\omega$ (eV)",    min=x2min, max=x2max),
     y     = graph.axis.log(title=r"$\epsilon(i\omega)$", min=1, max=1e8)
 )
 
-omegap, gamma = 9/hbar, 0.035/hbar
+omegap, gamma = 9, 0.03
 drude = "y(x) = 1+%g/(x*(x+%g))" % (omegap**2, gamma)
 
 attrs = graph.style.line([gradient, style.linewidth.thick, attr.changelist([style.linestyle.solid, style.linestyle.dashed, style.linestyle.dashdotted])])
 
 g.plot([
-    graph.data.points(dalvit, x=1, y=2, title="Palik"),
-    graph.data.points(mixed,  x=1, y=2, title="Olmon + Palik"),
-    graph.data.function(drude, title="Drude")
+    graph.data.values(x=dalvit[:,0]*hbar, y=dalvit[:,1], title="Palik"),
+    graph.data.values(x=mixed[:,0]*hbar,  y=mixed[:,1],  title="Olmon (SC) + Palik"),
+    graph.data.function(drude, title=r"Drude")
 ], [attrs])
+
+
+g.text(4.25,2.75, r"$\omega_P=9\,$eV, $\gamma=30\,$meV")
+
 
 g.writePDFfile()
