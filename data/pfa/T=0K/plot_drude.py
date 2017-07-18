dcc456cd0a5c6b1e855a72fb63ad990b9a637a40
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-18 10:27:47 +0200

    update

--- data/pfa/T=0K/plot_drude.py
+++ data/pfa/T=0K/plot_drude.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pyx import *
 from glob import glob
 
@@ -10,37 +11,29 @@ def slurp(filenames):
                 if len(line) and line[0] != "#":
                     data.append(list(map(float, line.split(","))))
 
-    return list(sorted(data))
+    return np.array(sorted(data))
 
 
 if __name__ == "__main__":
-    desc = r"""$R=151.3\mu\mathrm{m}$,
-    $\omega_p  = 8.9 \mathrm{eV}$,
-    $\gamma    = 0.0357 \mathrm{eV}$
-    """
-    epsrel = r"$\varepsilon_\mathrm{rel} = 3\cdot 10^{-5}$"
-
     # read data
-    data = slurp(glob("drude/*.out"))
+    data = slurp(glob("drude/eta10/*.out"))
+
+    LbyR = data[:,0]
+    F    = data[:,5]
 
     # use LaTeX for Pyx
     text.set(text.LatexRunner)
 
     g = graph.graphxy(
         width = 8,
-        x     = graph.axis.log(title=r"$L/R$", min=0.00095),
-        y     = graph.axis.lin(title=r"$\mathcal{F}/\mathcal{F}_\mathrm{PFA}$", max=1)
+        x     = graph.axis.log(title=r"$L/R$"),
+        y     = graph.axis.log(title=r"$-\mathcal{F}$")
     )
 
     # L/R, ldim, F_PFA(T=0)*(L+R)/ħc), F(T=0)*(L+R)/(ħc), F/F_pfa
     g.plot(
-       graph.data.points(data, x=1, y=5),
+       graph.data.values(x=LbyR, y=-F),
        [graph.style.line(), graph.style.symbol(graph.style.symbol.circle, size=0.07)]
     )
-
-    g.finish()
-
-    g.text(0.3, 0.8, desc,   [text.halign.flushright])
-    g.text(0.3, 0.3, epsrel, [text.halign.flushright])
 
     g.writePDFfile()
