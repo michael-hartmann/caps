62401ed39334f776666b05ef75d9006e84a7c912
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-18 10:32:09 +0200

    add notes for presentation about optical data

---
+++ theory/jc/optical_data/img/diff.py
@@ -0,0 +1,16 @@
+import numpy as np
+from pyx import *
+
+text.set(text.LatexRunner)
+
+data = np.loadtxt("diff.csv")
+
+g = graph.graphxy(
+    width = 8,
+    x     = graph.axis.lin(title=r"$L$ (nm)", min=150, max=800),
+    y     = graph.axis.lin(title=r"$F_\mathrm{Palik}/F_\mathrm{Olmon}$")
+)
+
+g.plot(graph.data.points(data, x=1, y=2), [graph.style.line()])
+
+g.writePDFfile()
