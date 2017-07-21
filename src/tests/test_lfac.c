bb9a07b26042010305776f4e20be8e545cef68d8
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-21 10:57:34 +0200

    increase number of tests

--- src/tests/test_lfac.c
+++ src/tests/test_lfac.c
@@ -8,7 +8,7 @@ int test_lfac(void)
     unittest_t test;
     unittest_init(&test, "lfac", "Test log factorial", 1e-15);
 
-    for(int i = 0; i < 200000; i++)
+    for(int i = 0; i < 800000; i++)
         AssertAlmostEqual(&test, lfac(i), lgamma(1+i));
 
     return test_results(&test, stderr);
