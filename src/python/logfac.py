0f6dcf08c4a06aeea80a6c5d9e4869abd42be229
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-25 14:11:00 +0200

    fix identation of generated code

--- src/python/logfac.py
+++ src/python/logfac.py
@@ -24,8 +24,8 @@ if __name__ == "__main__":
 #ifndef LOGFAC_H
 #define LOGFAC_H
 
-    double lfac(unsigned int n) __attribute__ ((pure));
-    double logi(unsigned int x) __attribute__ ((pure));
+double lfac(unsigned int n) __attribute__ ((pure));
+double logi(unsigned int x) __attribute__ ((pure));
 
 #endif"""
 
