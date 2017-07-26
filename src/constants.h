89432b27082b8fe8cb5578cb3380805480c07f74
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-26 10:28:12 +0200

    move constants to separate file

---
+++ src/constants.h
@@ -0,0 +1,20 @@
+#ifndef M_PI
+#define M_PI 3.14159265358979323846 /**< π */
+#endif
+
+#ifndef M_LOG2
+#define M_LOG2 0.6931471805599453 /**< log(2) */
+#endif
+
+#ifndef M_LOGPI
+#define M_LOGPI 1.1447298858494002 /**< log(π) */
+#endif
+
+#ifndef M_GM
+#define M_GM 1.618033988749895 /**< golden mean, (1+√5)/2 */
+#endif
+
+#define CASIMIR_hbar    1.0545718e-34   /**< reduced Planck constant [m² kg / s] */
+#define CASIMIR_hbar_eV 6.582119514e-16 /**< reduced Planck constant [eV s] */
+#define CASIMIR_kB      1.38064852e-23  /**< Boltzman constant [m² kg / ( K s² )] */
+#define CASIMIR_c       299792458.      /**< speed of light [m/s] */
