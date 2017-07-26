f745097d446c1409dd4194064a2781621a1b00d2
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-26 11:11:26 +0200

    rename function

--- src/plm.h
+++ src/plm.h
@@ -5,6 +5,8 @@ double Plm(int l, int m, double x) __attribute__ ((pure));
 double Plm_upwards(int l, int m, double x) __attribute__ ((pure));
 double Plm_downwards(int l, int m, double x) __attribute__ ((pure));
 
+double plm_continued_fraction(const int l, const int m, const double x) __attribute__ ((pure));
+
 double Plm_estimate(int l, int m, double x) __attribute__ ((pure));
 double Pl(int l, double x);
 
