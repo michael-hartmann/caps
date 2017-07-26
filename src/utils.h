9c15c0ac3877d079258e25e642d8fab1e1087042
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-26 12:48:04 +0200

    improve documentation

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,17 @@
+/**
+ * @file   utils.h
+ * @author Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
+ * @date   July, 2017
+ * @brief  wrappers for malloc, calloc and realloc, assert-like macros, now()-function
+ */
+
 #ifndef __UTILS_H
 #define __UTILS_H
 
 #include <stdlib.h>
 #include <stdio.h>
 
+/*! name of compile */
 #if defined(__clang__)
     #define COMPILER "clang/llvm"
 #elif defined(__ICC) || defined(__INTEL_COMPILER)
@@ -14,10 +22,13 @@
     #define COMPILER "unknown"
 #endif
 
-#define TERMINATE(cond, ...) if(cond) { fprintf(stderr, "Fatal error: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, " (in %s, %s:%d)\n", __func__, __FILE__, __LINE__); exit(1); }
+/*! macro similar to assert that prints a warning to stderr and aborts */
+#define TERMINATE(cond, ...) if(cond) { fprintf(stderr, "Fatal error: "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, " (in %s, %s:%d)\n", __func__, __FILE__, __LINE__); abort(); }
+
+/*! macro similar to assert that prints a warning to stderr */
 #define WARN(cond, ...)      if(cond) { fprintf(stderr, "Warning: ");     fprintf(stderr, __VA_ARGS__); fprintf(stderr, " (in %s, %s:%d)\n", __func__, __FILE__, __LINE__); }
 
-
+/*! macro for free that sets pointer p to NULL after freeing memory */
 #define xfree(p) do { _xfree(p); p = NULL; } while(0)
 
 double now(void);
