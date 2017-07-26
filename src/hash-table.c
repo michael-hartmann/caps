62ba23242000ba9ce632ed01789b12e7f0224e87
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-07-26 10:46:45 +0200

    reformat header-comment

--- src/hash-table.c
+++ src/hash-table.c
@@ -1,34 +1,30 @@
 /*
+ * Copyright (c) 2005-2008, Simon Howard
 
-Copyright (c) 2005-2008, Simon Howard
+ * This file has been modified by
+ *     Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>, October 2016
+ * in order to adapt the implementation of Simon Howard to libcasimir.
 
-This file has been modified by
-    Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>, October 2016
-in order to adapt the implementation of Simon Howard to libcasimir.
-
-1) Keys are plain numbers (uint64_t):
-    As the keys are plain numbers, the user doesn't have to provide a hash
-    function or a function to compare two keys.
-
-2) The default size of the hash table is larger.
-
-Permission to use, copy, modify, and/or distribute this software
-for any purpose with or without fee is hereby granted, provided
-that the above copyright notice and this permission notice appear
-in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
-WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
-CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
-NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
-CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
+ * 1) Keys are plain numbers (uint64_t):
+ *    As the keys are plain numbers, the user doesn't have to provide a hash
+ *    function or a function to compare two keys.
+ *
+ * 2) The default size of the hash table is larger.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided
+ * that the above copyright notice and this permission notice appear
+ * in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
-/* Hash table implementation */
 
 #include <stdlib.h>
 #include <string.h>
