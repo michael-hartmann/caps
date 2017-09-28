1e6fb02400203158c70a6d6073cf651d73fa0845
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-09-28 13:21:16 -0300

    use Gaunt coefficients to speed up computation

--- src/casimir_scalar.h
+++ src/casimir_scalar.h
@@ -2,20 +2,32 @@
 #define CASIMIR_SCALAR
 
 typedef struct {
-    double LbyR;
-    double xi_; /* xi_ = xi*(L+R)/c */
-    double epsrel;
-    int m;
-    char X,Y;
-    double *cache_rs;
-} args_t;
-
-typedef struct {
     double max;
-    double tau;
-    int l1,l2,m;
+    double xi_;
+    int l,m;
 } integrand_t;
 
+typedef struct {
+    double xi_;    /*< xi_ = xi*(L+R)/c */
+    double epsrel; /*< relative error for numerical integration */
+    int m;         /*< azimutal quantum number m */
+    double *K;
+} integration_t;
+
+typedef struct {
+    double LbyR;   /*< L/R */
+    int ldim;      /*< dimension of vector space */
+    int m;         /*< azimuthal quantum number m */
+    double xi_;    /*< xi*R/c */
+    double epsrel; /*< relative accuracy for integration */
+    char X;        /*< boundary condition on plate (D or N) */
+    char Y;        /*< boundary condition on sphere (D or N) */
+    double *rs;    /*< cache for rs */
+    integration_t *integration;
+} args_t;
+
 double logdetD(double LbyR, double xi_, int m, int ldim, char X, char Y, double epsrel);
+integration_t *integration_init(int m, double xi_, int ldim, double epsrel);
+void integration_free(integration_t *self);
 
 #endif
