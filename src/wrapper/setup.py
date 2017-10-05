6d28f9c8dc2a921f61379781d7afa41fb88fc966
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-10-05 15:55:53 +0200

    adjust setup.py file

--- src/wrapper/setup.py
+++ src/wrapper/setup.py
@@ -5,7 +5,7 @@ from distutils.extension import Extension
 extension = Extension("libcasimir", ["libcasimir.pyx"],
         include_dirs = [".."],
         libraries = ["casimir", "hodlr"],
-        library_dirs = ["..", "/home/michael/git/hodlr_wrapper"])
+)
 
 setup(
     ext_modules = cythonize(extension)
