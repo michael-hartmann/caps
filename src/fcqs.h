bcdbd277cdb50a91229cb74cb43c57f8e5215a3c
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2017-12-21 09:29:41 +0100

    add finite intervals, fix bug
    This commit adds a routine to compute the integral over a finite interval.
    Also, it fixes a bug where the pointer epsrel was checked if it was negative
    instead of the value *epsrel.

--- src/fcqs.h
+++ src/fcqs.h
@@ -2,5 +2,6 @@
 #define FCQS_H
 
 double fcgs_semiinf(double f(double, void *), void *args, double *epsrel, int *neval, double L, int *ier);
+double fcgs_finite(double f(double, void *), void *args, double a, double b, double *epsrel, int *neval, int *ier);
 
 #endif
