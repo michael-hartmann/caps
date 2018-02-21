396a1e71c541644ae74e933d7853b8df1338ebfb
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-02-21 14:05:01 +0100

    add plot for energy

--- data/pfa/T=300K/plot.py
+++ data/pfa/T=300K/plot.py
@@ -3,7 +3,7 @@ from sys import stderr
 from glob import glob
 import numpy as np
 import scipy.interpolate
-from bimonte import F_HT_cut
+from bimonte import F_HT_cut, E_HT_cut
 
 def cut_E(value):
     points = []
@@ -49,10 +49,7 @@ def cut_F(value):
     return np.array(sorted(points))
 
 
-if __name__ == "__main__":
-    text.set(text.LatexRunner)
-
-    #text.preamble(r"\usepackage{textcomp}")
+def plotF(out="pfaF.pdf"):
     values = (0.01, 0.005, 0.003) # 1%, 0.5%, 0.3%
 
     xmin,xmax = 1, 300
@@ -84,4 +81,46 @@ if __name__ == "__main__":
 
     g.plot(plots_ht, [graph.style.line([style.linestyle.dashed, color.cmyk.Maroon])])
 
-    g.writePDFfile()
+    g.writePDFfile(out)
+
+
+def plotE(out="pfaE.pdf"):
+    values = (0.005,0.01,0.02) # 1%
+
+    xmin,xmax = 1, 300
+    ymin,ymax = 5e0, 1e4
+    xtitle = r"$R$ in $\mu\mathrm{m}$"
+    ytitle = r"$L$ in $\mathrm{nm}$"
+
+    g = graph.graphxy(
+        width = 8,
+        key   = graph.key.key(pos="tl"),
+        x     = graph.axis.log(title=xtitle, min=xmin, max=xmax),
+        y     = graph.axis.log(title=ytitle, min=ymin, max=ymax)
+    )
+
+    plots = []
+    for value in values:
+        title = r"$1-E/E_\mathrm{PFA} = %g$" % (value*100) + r"\%"
+        #title = r"$%g$" % (value*100) + r"\%"
+        data = cut_E(value)
+        plots.append(graph.data.points(data, x=1, y=2, title=title))
+
+    g.plot(plots, [graph.style.symbol(graph.style.symbol.changecircle, size=0.1), graph.style.line([style.linestyle.solid])])
+
+    plots_ht = []
+    for value in values:
+        LbyR = E_HT_cut(value)
+        cmd = "y(x)=x*%.8g*1e3" % LbyR
+        plots_ht.append(graph.data.function(cmd, min=20, title=None))
+
+    g.plot(plots_ht, [graph.style.line([style.linestyle.dashed, color.cmyk.Maroon])])
+
+    g.writePDFfile(out)
+
+
+if __name__ == "__main__":
+    text.set(text.LatexRunner)
+
+    plotF()
+    plotE()
