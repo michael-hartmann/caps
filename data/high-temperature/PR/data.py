1b62b34030f189be1987e0b12bcba3f90d4413fd
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-03-20 14:53:39 +0100

    add perfect reflectors

---
+++ data/high-temperature/PR/data.py
@@ -0,0 +1,14 @@
+import numpy as np
+from glob import glob
+
+data = []
+for filename in glob("raw-*.out"):
+    LbyR, L, R, ldim, F_drude, F_PR = np.loadtxt(filename, delimiter=",")
+    data.append((1/LbyR, F_drude, F_PR))
+
+print("# high-temperature limit for perfect reflectors")
+print("#")
+print("# R/L, F_Drude/(kB*T), F_PR/(kB*T)")
+
+for RbyL, drude, pr in sorted(data):
+    print("%.8g, %.14g, %.14g" % (RbyL, drude, pr))
