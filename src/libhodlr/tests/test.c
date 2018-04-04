fcfc1fcda3a6626281b6bd71997af8467e95a09c
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-04-04 10:45:06 +0200

    support colours, add more information

--- src/libhodlr/tests/test.c
+++ src/libhodlr/tests/test.c
@@ -1,9 +1,29 @@
+/* Test HODLR library
+ *
+ * Test of the HODLR library and the wrapper. We use the kernel for the MM
+ * polarization block in the high-temperature limit. For m=0 the result is
+ * known analytically. [1]
+ *
+ * We compare the numerical result with the exact expression for different
+ * aspect ratios R/L. The dimension of the vector space is chosen as
+ * ldim=10*R/L.
+ *
+ * [1] Giuseppe Bimonte, Classical Casimir interaction of a perfectly
+ *     conducting sphere and plate, Phys. Rev. D 95, 065004 (2017)
+ *     https://doi.org/10.1103/PhysRevD.95.065004
+ */
+
 #include <stdio.h>
 #include <math.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 #include <hodlr.h>
+
+/* ANSI codes for colour */
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
 
 /* return time in seconds since 1970 */
 double now(void)
@@ -13,7 +33,7 @@ double now(void)
     return tv.tv_sec + tv.tv_usec*1e-6;
 }
 
-/* analytical solution of kernel */
+/* analytical solution of kernel, see [1] */
 double analytical(double RbyL)
 {
     const int lmax = 20000;
@@ -38,6 +58,7 @@ double kernel(int i, int j, void *args)
 
 int main(int argc, char *argv[])
 {
+    char *status[] = { KGRN "ok" KNRM, KRED "failed" KNRM };
     unsigned int nLeaf = 100;
     double tolerance = 1e-10;
     int is_symmetric = 1;
@@ -52,6 +73,7 @@ int main(int argc, char *argv[])
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
 
+    printf("# R/L, analytical, HODLR, relative error, status\n");
     for(int i = 0; i < npts; i++)
     {
         double RbyL = RbyL_start*exp(i*log(RbyL_stop/RbyL_start)/(npts-1));
@@ -63,8 +85,8 @@ int main(int argc, char *argv[])
         t = now()-t;
         double exact = analytical(RbyL);
 
-        double error = 1-fabs(logdet/exact);
-        printf("%g, %.14g, %g, %s\n", RbyL, error, t, error < 2e-9 ? "ok" : "failed");
+        double relerr = 1-fabs(logdet/exact);
+        printf("%g, %.14g, %.14g, %.14g, %.5g, %s\n", RbyL, exact, logdet, relerr, t, relerr < 2e-9 ? status[0] : status[1]);
     }
 
     return 0;
