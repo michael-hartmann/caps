627c100616a311eadf81dadab1bd3f12a3d6ac19
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-04-18 10:38:13 +0200

    remove unused function lnPlm_estimate

--- src/plm.h
+++ src/plm.h
@@ -7,7 +7,6 @@ double lnPlm_downwards(int l, int m, double x) __attribute__ ((pure));
 
 double Plm_continued_fraction(const long l, const long m, const double x) __attribute__ ((pure));
 
-double lnPlm_estimate(int l, int m, double x) __attribute__ ((pure));
 double lnPl(int l, double x);
 
 double dlnPlm(int l, int m, double x, double *d2lnPlm);
