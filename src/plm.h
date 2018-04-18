bf36336cf9891545c330ddb50f81df76025cd95c
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-04-18 10:40:09 +0200

    mark lnPl as a pure function

--- src/plm.h
+++ src/plm.h
@@ -7,7 +7,7 @@ double lnPlm_downwards(int l, int m, double x) __attribute__ ((pure));
 
 double Plm_continued_fraction(const long l, const long m, const double x) __attribute__ ((pure));
 
-double lnPl(int l, double x);
+double lnPl(int l, double x) __attribute__ ((pure));
 
 double dlnPlm(int l, int m, double x, double *d2lnPlm);
 
