9040f258ee194af34c9a7d1da98d4647a710388f
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-04-23 15:17:49 +0200

    add script to test spence function

---
+++ src/python/test_spence.py
@@ -0,0 +1,24 @@
+import numpy as np
+import mpmath as mp
+from spence import Li2
+
+mp.dps = 50
+
+def Li2_mpmath(x):
+    return mp.polylog(2,mp.mpf(x))
+
+maxerr = 0
+x_maxerr = 0
+for x in np.linspace(0,1,25000):
+    if x == 0:
+        continue
+
+    exact = Li2_mpmath(x)
+    fast  = Li2(x)
+    err   = abs(1-fast/exact) # relative error
+    
+    if err >= maxerr:
+        maxerr = err
+        x_maxerr = x
+
+print("Maximum relative error %g at x=%g" % (maxerr, x_maxerr))
