ecc26d265f86d9af65cd0d7e90c9a8bf4bc1d0a0
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-05-24 10:36:13 +0200

    use argparse and check radii

--- data/decca/plot.py
+++ data/decca/plot.py
@@ -1,15 +1,32 @@
+import argparse
 import numpy as np
-from sys import argv
+from sys import stderr, argv
 from pyx import *
+
+def get_radius(filename):
+    with open(filename) as f:
+        for line in f:
+            if "# R = " in line and "nm" in line:
+                return int(line.split("=")[1].strip()[:-2])
+
+    assert False
+
+parser = argparse.ArgumentParser(description="plot data")
+parser.add_argument("-e", "--experimental",  help="experimental data", type=str)
+parser.add_argument("-n", "--numerical",     help="numerical data", type=str)
+parser.add_argument("-o", "--output", type=str, default="plot.pdf")
+args = parser.parse_args()
 
 text.set(text.LatexRunner)
 
-filename_exp = argv[1]
-filename_num = argv[2]
-R = argv[3]
+experiment = np.loadtxt(args.experimental, delimiter=",")
+numerics   = np.loadtxt(args.numerical,    delimiter=",")
 
-experiment = np.loadtxt(filename_exp, delimiter=",")
-numerics   = np.loadtxt(filename_num, delimiter=",")
+R_exp = get_radius(args.experimental)
+R_num = get_radius(args.numerical)
+if R_exp != R_num:
+    print("radii of experimental and numerical data differ: %d (exp) vs %d (num)" % (R_exp, R_num))
+    exit(1)
 
 g = graph.graphxy(
     width = 11,
@@ -26,6 +43,6 @@ g.plot(graph.data.values(x=numerics[:,0], y=numerics[:,2], title="Drude (PFA)"),
 g.plot(graph.data.values(x=numerics[:,0], y=numerics[:,3], title="plasma (exact)"), [graph.style.line([color.cmyk.BrickRed])])
 g.plot(graph.data.values(x=numerics[:,0], y=numerics[:,4], title="plasma (PFA)"), [graph.style.line([color.cmyk.BrickRed, style.linestyle.dashed])])
 
-g.text(10.9,0.3, r"$R=%s\,\mu\mathrm{m}$" % R, [text.halign.right])
+g.text(10.9,0.3, r"$R=%.1f\,\mu\mathrm{m}$" % (R_num/1000), [text.halign.right])
 
-g.writePDFfile()
+g.writePDFfile(args.output)
