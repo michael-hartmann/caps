a2f86c72451ade8c9e27c47444c1dfdfcf220556
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-06-20 09:36:27 +0200

    update

--- src/python/teo/teo.py
+++ src/python/teo/teo.py
@@ -1,46 +1,51 @@
 import ctypes
 from math import sqrt, pi
-from itertools import count
 from scipy.integrate import dblquad
+from sys import path
+path.append("..")
+
+from PFA import PFA, hbarc, c, hbar_eV
 
 clib = ctypes.CDLL("./integrand.so")
 clib.integrand.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
 clib.integrand.restype = ctypes.c_double
 
-c       = 299792458       # speed of light [m/s]
-hbar_eV = 6.582119514e-16 # hbar [eV s / rad]
-
-def epsilon(xi, omegap, gamma):
-    xi_eV = xi*hbar_eV
-    return 1+omegap**2/(xi_eV*(xi_eV+gamma))
-
 def integrand(x, xi_, L, R, omegap, gamma):
-    assert x >= xi_
-    e = L/R # L/R
-
     xi = xi_*c/L
-    eps = epsilon(xi, omegap, gamma)
 
-    return clib.integrand(xi_, x, eps, e)
+    xi_eV = xi*hbar_eV
+    eps = 1+omegap**2/(xi_eV*(xi_eV+gamma))
 
-omegap = 9
-gamma = 0 #0.035
-R = 150e-6
-L = 300e-9
+    return clib.integrand(xi_, x, eps, L/R)
 
-#def f(x,y,L,R,omegap,gamma):
-#    l = y
-#    tau = 2*e*l/x
-#    dx = 2*e/tau
-#    return dx*integrand(tau, l, L, R, omegap, gamma)
+def theta_teo(L,R,omegap=9,gamma=0.035):
+    epsm1 = lambda xi: (omegap/hbar_eV)**2/(xi*(xi+gamma/hbar_eV))
+    pfa = PFA(R, 0, epsm1)
+    E0 = pfa.E(L)*(L+R)/hbarc
 
-I, err = dblquad(integrand, 0, float("inf"), lambda x: x, lambda x: float("inf"), args=(L,R,omegap,gamma), epsrel=1e-8)
+    I, err = dblquad(integrand, 0, float("inf"), lambda x: x, lambda x: float("inf"), args=(L,R,omegap,gamma), epsrel=1e-9)
+    f = R/L*(1+R/L)/(4*pi)
+    E1 = -I*f
 
-f = R/L*(1+R/L)/(4*pi)
-E0 = -9036.32256859 #-31147.0818086
-E1 = -I*f
-#print("pfa", pfa)
+    theta = R/L*E1/E0
 
-print(omegap/hbar_eV*L/c)
-theta = R/L*E1/E0
-print(theta)
+    return theta
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Compute PFA in the plane-sphere geometry")
+    parser.add_argument("--omegap", type=float, default=9, help="plasma frequency in eV")
+    parser.add_argument("--gamma", type=float, default=0, help="relaxation frequency in eV")
+    parser.add_argument("-L", type=float, required=True, help="separation between sphere and plate in m")
+    parser.add_argument("-R", type=float, required=True, help="radius of sphere in m")
+    args = parser.parse_args()
+
+    print("L      = %g" % args.L)
+    print("R      = %g" % args.R)
+    print("omegap = %g" % args.omegap)
+    print("gamma  = %g" % args.gamma)
+
+    theta = theta_teo(args.L, args.R, args.omegap, args.gamma)
+    print("theta  = %.15g" % theta)
