41665a19f259a75df3bd3428aaeb8a6e3aaecfcd
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-06-20 10:24:55 +0200

    docstring

--- src/python/teo/teo.py
+++ src/python/teo/teo.py
@@ -6,25 +6,44 @@ path.append("..")
 
 from PFA import PFA, hbarc, c, hbar_eV
 
-clib = ctypes.CDLL("./integrand.so")
-clib.integrand.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
-clib.integrand.restype = ctypes.c_double
+def theta_teo(L, R, omegap=9, gamma=0.035, epsrel=1e-11):
+    """Compute linear correction Θ_1 for T=0
 
-def integrand(x, xi_, L, R, omegap, gamma):
-    xi = xi_*c/L
+    The linear correction is given as
+        E ≈ E_0 + E_1 = E_0*(1+E_1/E_0) = E_0*(1+Θ_1*x)
+    where x=L/R and E_0 corresponds to the PFA result.
 
-    xi_eV = xi*hbar_eV
-    eps = 1+omegap**2/(xi_eV*(xi_eV+gamma))
+    Parameters
+    ----------
+        L:      separation between sphere and plate in m
+        R:      radius of the sphere in m
+        omegap: plasma frequency in eV
+        gamma:  relaxation frequency in eV
+        epsrel: relative accuracy for integration
 
-    return clib.integrand(xi_, x, eps, L/R)
+    Returns
+    -------
+    Θ_1: linear correction
+    """
 
+    # open library
+    clib = ctypes.CDLL("./integrand.so")
+    clib.integrand.argtypes = (ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double)
+    clib.integrand.restype = ctypes.c_double
 
-def theta_teo(L,R,omegap=9,gamma=0.035):
     epsm1 = lambda xi: (omegap/hbar_eV)**2/(xi*(xi+gamma/hbar_eV))
     pfa = PFA(R, 0, epsm1)
     E0 = pfa.E(L)*(L+R)/hbarc
 
-    I, err = dblquad(integrand, 0, float("inf"), lambda x: x, lambda x: float("inf"), args=(L,R,omegap,gamma), epsrel=1e-11)
+    omegap_rads = omegap/hbar_eV
+    gamma_rads  = gamma/hbar_eV
+
+    def integrand(x, xi_, L, R, omegap, gamma):
+        xi = xi_*c/L
+        eps = 1+omegap_rads**2/(xi*(xi+gamma_rads))
+        return clib.integrand(xi_, x, eps, L/R)
+
+    I, err = dblquad(integrand, 0, float("inf"), lambda x: x, lambda x: float("inf"), args=(L,R,omegap,gamma), epsrel=epsrel)
     f = R/L*(1+R/L)/(4*pi)
     E1 = -I*f
 
