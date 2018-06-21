b67f46a009f7e5c7eb24fa019fa9f08772e8578c
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-06-21 10:20:52 +0200

    add epsrel/absrel, add comments

--- src/python/teo/teo.py
+++ src/python/teo/teo.py
@@ -3,10 +3,36 @@ from math import pi
 from scipy.integrate import dblquad
 from sys import path
 path.append("..")
+from uncertainties import ufloat
 
 from PFA import PFA, hbarc, c, hbar_eV
 
-def theta_teo(L, R, omegap=9, gamma=0.035, epsrel=1e-11):
+# This script computes the linear correction Θ_1 at T=0. The correction looks
+# like
+#       E = E_PFA (1+L/R*Θ_1)
+# where Θ_1 is a function of L but independent of R.
+#
+# To compute the linear correction, we use the formulas implemented in Ref [1].
+# The Casimir energy is given as
+#       E = E0 + E1
+# where E0 corresponds to the PFA result, E0=E_PFA, and E1 is the correction.
+# The linear term can be computed by means of
+#       Θ_1 = R/L * E1/E0.
+# The linear correction Θ_1 is independent and one gets the same value for Θ_1
+# independent of the choice of R. However, the integration error gets
+# multiplied by R/L. For this reason high aspect ratios increase the
+# uncertainty of Θ_1. On the other hand, the integration has problems when the
+# aspect ratio is not sufficiently high.
+#
+# The actual integrand contains a lot of computations and an infinite sum. For
+# performance reasons, it is implemented in a C library, see integrand.c. The
+# library is called using ctypes.
+#
+# [1] Teo, Material dependence of Casimir interaction between a sphere and a
+# plate: First analytic correction beyond proximity force approximation,
+# https://doi.org/10.1103/PhysRevD.88.045019
+
+def theta_teo(L, R, omegap=9, gamma=0.035, epsrel=1e-11, epsabs=1e-8):
     """Compute linear correction Θ_1 for T=0
 
     The linear correction is given as
@@ -19,7 +45,8 @@ def theta_teo(L, R, omegap=9, gamma=0.035, epsrel=1e-11):
         R:      radius of the sphere in m
         omegap: plasma frequency in eV
         gamma:  relaxation frequency in eV
-        epsrel: relative accuracy for integration
+        epsrel: relative tolerance for integration
+        epsabs: absolute tolerance for integration
 
     Returns
     -------
@@ -43,7 +70,8 @@ def theta_teo(L, R, omegap=9, gamma=0.035, epsrel=1e-11):
         eps = 1+omegap_rads**2/(xi*(xi+gamma_rads))
         return clib.integrand(xi_, x, eps, L/R)
 
-    I, err = dblquad(integrand, 0, float("inf"), lambda x: x, lambda x: float("inf"), args=(L,R,omegap,gamma), epsrel=epsrel)
+    I, err = dblquad(integrand, 0, float("inf"), lambda x: x, lambda x: float("inf"), args=(L,R,omegap,gamma), epsrel=epsrel, epsabs=epsabs)
+    I = ufloat(I,err)
     f = R/L*(1+R/L)/(4*pi)
     E1 = -I*f
 
@@ -58,6 +86,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Compute linear correction to PFA in the plane-sphere geometry for T=0")
     parser.add_argument("--omegap", type=float, default=9, help="plasma frequency in eV")
     parser.add_argument("--gamma", type=float, default=0, help="relaxation frequency in eV")
+    parser.add_argument("--epsrel", type=float, default=1e-11, help="relative tolerance for integration")
+    parser.add_argument("--epsabs", type=float, default=1e-8, help="absolute tolerance for integration")
     parser.add_argument("-L", type=float, required=True, help="separation between sphere and plate in m")
     parser.add_argument("-R", type=float, required=True, help="radius of sphere in m")
     args = parser.parse_args()
@@ -67,5 +97,5 @@ if __name__ == "__main__":
     print("omegap = %g" % args.omegap)
     print("gamma  = %g" % args.gamma)
 
-    theta = theta_teo(args.L, args.R, args.omegap, args.gamma)
-    print("theta  = %.15g" % theta)
+    theta = theta_teo(args.L, args.R, omegap=args.omegap, gamma=args.gamma, epsrel=args.epsrel, epsabs=args.epsabs)
+    print("theta  = ", theta)
