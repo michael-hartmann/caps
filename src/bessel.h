298f129610503001292f97f5e2892198860123b3
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-08-30 13:44:12 +0200

    remove double ;

--- src/bessel.h
+++ src/bessel.h
@@ -4,15 +4,15 @@
 double besselI0(double x);
 double besselI0e(double x);
 
-double besselI1(double x) __attribute__ ((pure));;
-double besselI1e(double x) __attribute__ ((pure));;
+double besselI1(double x) __attribute__ ((pure));
+double besselI1e(double x) __attribute__ ((pure));
 
-double besselI(int n, double x) __attribute__ ((pure));;
+double besselI(int n, double x) __attribute__ ((pure));
 
-double bessel_continued_fraction(int nu, double x) __attribute__ ((pure));;
+double bessel_continued_fraction(int nu, double x) __attribute__ ((pure));
 
-double bessel_lnInu(int nu, double x) __attribute__ ((pure));;
-double bessel_lnKnu(int nu, double x) __attribute__ ((pure));;
+double bessel_lnInu(int nu, double x) __attribute__ ((pure));
+double bessel_lnKnu(int nu, double x) __attribute__ ((pure));
 
 void bessel_lnInuKnu(int nu, const double x, double *lnInu_p, double *lnKnu_p);
 #endif
