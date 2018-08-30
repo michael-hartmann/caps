0fb3eb5cdaa0358beff4fc2d277b3c6b21cea0a0
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-08-30 10:39:50 +0200

    remove resume feature, cleanup

--- src/casimir.h
+++ src/casimir.h
@@ -20,11 +20,9 @@ typedef struct {
     casimir_task_t **tasks;
     int determinants;
     char filename[512];
-    double cache[4096][2];
-    int cache_elems;
 } casimir_mpi_t;
 
-casimir_mpi_t *casimir_mpi_init(double L, double R, double T, char *filename, char *resume, double omegap, double gamma_, int ldim, double cutoff, double iepsrel, int cores, bool verbose);
+casimir_mpi_t *casimir_mpi_init(double L, double R, double T, char *filename, double omegap, double gamma_, int ldim, double cutoff, double iepsrel, int cores, bool verbose);
 void casimir_mpi_free(casimir_mpi_t *self);
 int casimir_mpi_submit(casimir_mpi_t *self, int index, double xi, int m);
 int casimir_mpi_retrieve(casimir_mpi_t *self, casimir_task_t **task_out);
