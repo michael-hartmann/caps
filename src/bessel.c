680a2874b4f5b534c6e514edcd99d5823b2e4714
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-10-22 13:32:53 +0200

    add functions besselK0, besselK0e, besselK1, besselK1e

--- src/bessel.c
+++ src/bessel.c
@@ -2,7 +2,7 @@
  * @file   bessel.c
  * @author Stephen L. Moshier, Cephes Math Library Release 2.8, June 2000
  * @author Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
- * @date   July, 2017
+ * @date   July, 2018
  * @brief  Computation of Bessel functions
  */
 
@@ -18,7 +18,7 @@
  *
  * lim(x->0){ exp(-x) I0(x) } = 1.
  */
-static double A0[] =
+static double I0_A[] =
 {
     -4.41534164647933937950E-18,
      3.33079451882223809783E-17,
@@ -57,7 +57,7 @@ static double A0[] =
  *
  * lim(x->inf){ exp(-x) sqrt(x) I0(x) } = 1/sqrt(2pi).
  */
-static double B0[] =
+static double I0_B[] =
 {
     -7.23318048787475395456E-18,
     -4.83050448594418207126E-18,
@@ -86,12 +86,65 @@ static double B0[] =
      8.04490411014108831608E-1
 };
 
+/* Chebyshev coefficients for K0(x) + log(x/2) I0(x)
+ * in the interval [0,2].  The odd order coefficients are all
+ * zero; only the even order coefficients are listed.
+ *
+ * lim(x->0){ K0(x) + log(x/2) I0(x) } = -EUL.
+ */
+static double K0_A[] =
+{
+    1.37446543561352307156E-16,
+    4.25981614279661018399E-14,
+    1.03496952576338420167E-11,
+    1.90451637722020886025E-9,
+    2.53479107902614945675E-7,
+    2.28621210311945178607E-5,
+    1.26461541144692592338E-3,
+    3.59799365153615016266E-2,
+    3.44289899924628486886E-1,
+    -5.35327393233902768720E-1
+};
+
+/* Chebyshev coefficients for exp(x) sqrt(x) K0(x)
+ * in the inverted interval [2,infinity].
+ *
+ * lim(x->inf){ exp(x) sqrt(x) K0(x) } = sqrt(pi/2).
+ */
+static double K0_B[] = {
+     5.30043377268626276149E-18,
+    -1.64758043015242134646E-17,
+     5.21039150503902756861E-17,
+    -1.67823109680541210385E-16,
+     5.51205597852431940784E-16,
+    -1.84859337734377901440E-15,
+     6.34007647740507060557E-15,
+    -2.22751332699166985548E-14,
+     8.03289077536357521100E-14,
+    -2.98009692317273043925E-13,
+     1.14034058820847496303E-12,
+    -4.51459788337394416547E-12,
+     1.85594911495471785253E-11,
+    -7.95748924447710747776E-11,
+     3.57739728140030116597E-10,
+    -1.69753450938905987466E-9,
+     8.57403401741422608519E-9,
+    -4.66048989768794782956E-8,
+     2.76681363944501510342E-7,
+    -1.83175552271911948767E-6,
+     1.39498137188764993662E-5,
+    -1.28495495816278026384E-4,
+     1.56988388573005337491E-3,
+    -3.14481013119645005427E-2,
+     2.44030308206595545468E0
+};
+
 /* Chebyshev coefficients for exp(-x) I1(x) / x
  * in the interval [0,8].
  *
  * lim(x->0){ exp(-x) I1(x) / x } = 1/2.
  */
-static double A1[] =
+static double I1_A[] =
 {
      2.77791411276104639959E-18,
     -2.11142121435816608115E-17,
@@ -129,7 +182,7 @@ static double A1[] =
  *
  * lim(x->inf){ exp(-x) sqrt(x) I1(x) } = 1/sqrt(2pi).
  */
-static double B1[] =
+static double I1_B[] =
 {
      7.51729631084210481353E-18,
      4.41434832307170791151E-18,
@@ -156,6 +209,60 @@ static double B1[] =
     -1.10588938762623716291E-4,
     -9.76109749136146840777E-3,
      7.78576235018280120474E-1
+};
+
+/* Chebyshev coefficients for x(K1(x) - log(x/2) I1(x))
+ * in the interval [0,2].
+ *
+ * lim(x->0){ x(K1(x) - log(x/2) I1(x)) } = 1.
+ */
+static double K1_A[] =
+{
+    -7.02386347938628759343E-18,
+    -2.42744985051936593393E-15,
+    -6.66690169419932900609E-13,
+    -1.41148839263352776110E-10,
+    -2.21338763073472585583E-8,
+    -2.43340614156596823496E-6,
+    -1.73028895751305206302E-4,
+    -6.97572385963986435018E-3,
+    -1.22611180822657148235E-1,
+    -3.53155960776544875667E-1,
+     1.52530022733894777053E0
+};
+
+/* Chebyshev coefficients for exp(x) sqrt(x) K1(x)
+ * in the interval [2,infinity].
+ *
+ * lim(x->inf){ exp(x) sqrt(x) K1(x) } = sqrt(pi/2).
+ */
+static double K1_B[] =
+{
+    -5.75674448366501715755E-18,
+     1.79405087314755922667E-17,
+    -5.68946255844285935196E-17,
+     1.83809354436663880070E-16,
+    -6.05704724837331885336E-16,
+     2.03870316562433424052E-15,
+    -7.01983709041831346144E-15,
+     2.47715442448130437068E-14,
+    -8.97670518232499435011E-14,
+     3.34841966607842919884E-13,
+    -1.28917396095102890680E-12,
+     5.13963967348173025100E-12,
+    -2.12996783842756842877E-11,
+     9.21831518760500529508E-11,
+    -4.19035475934189648750E-10,
+     2.01504975519703286596E-9,
+    -1.03457624656780970260E-8,
+     5.74108412545004946722E-8,
+    -3.50196060308781257119E-7,
+     2.40648494783721712015E-6,
+    -1.93619797416608296024E-5,
+     1.95215518471351631108E-4,
+    -2.85781685962277938680E-3,
+     1.03923736576817238437E-1,
+     2.72062619048444266945E0
 };
 
 /** @brief Evaluate Chebyshev series
@@ -205,11 +312,7 @@ static double chbevl(double x, double array[], int n)
     return 0.5*(b0-b2);
 }
 
-/** @brief Modified Bessel function of order zero
- *
- * Returns modified Bessel function of order zero of the argument \f$x\f$.
- *
- * The function is defined as \f$I_0(x) = J_0(ix)\f$.
+/** @brief Modified Bessel function \f$I_0(x)\f$
  *
  * The range is partitioned into the two intervals \f$[0,8]\f$ and \f$(8, \infty)\f$.
  * Chebyshev polynomial expansions are employed in each interval.
@@ -225,19 +328,16 @@ double besselI0(double x)
     if(x <= 8.0)
     {
         double y = (x/2.0) - 2.0;
-        return(exp(x)*chbevl(y,A0,30));
+        return(exp(x)*chbevl(y,I0_A,30));
     }
 
-    return exp(x)*chbevl(32.0/x-2.0,B0,25)/sqrt(x);
+    return exp(x)*chbevl(32.0/x-2.0,I0_B,25)/sqrt(x);
 }
 
 
-/** @brief Modified Bessel function of order zero, exponentially scaled
+/** @brief Modified Bessel function \f$I_0(x)\f$, exponentially scaled
  *
- * Returns exponentially scaled modified Bessel function of order zero of the
- * argument \f$x\f$.
- *
- * The function is defined as \f$\tilde I_0(x) = \exp(-|x|) J_0(ix)\f$.
+ * The function returns \f$\exp(-|x|) I_0(x)\f$.
  *
  * See \ref besselI0.
  *
@@ -252,47 +352,95 @@ double besselI0e(double x)
     if(x <= 8.0)
     {
         double y = (x/2.0) - 2.0;
-        return chbevl(y,A0,30);
+        return chbevl(y,I0_A,30);
     }
 
-    return chbevl(32.0/x-2.0,B0,25)/sqrt(x);
+    return chbevl(32.0/x-2.0,I0_B,25)/sqrt(x);
+}
+
+/** @brief Modified Bessel function \f$K_0(x)\f$
+ *
+ * The range is partitioned into the two intervals \f$[0,8]\f$ and \f$(8,
+ * \infty)\f$.  Chebyshev polynomial expansions are employed in each interval.
+ *
+ * @param [in] x argument
+ * @retval K0 \f$K_0(x)\f$
+ */
+double besselK0(double x)
+{
+    double y,z;
+
+    if(x <= 0)
+        return NAN;
+
+    if(x <= 2.0)
+    {
+        y = x*x-2.0;
+        y = chbevl(y, K0_A, 10)-log(0.5*x)*besselI0(x);
+        return y;
+    }
+
+    z = 8.0/x-2.0;
+    y = exp(-x)*chbevl(z, K0_B, 25)/sqrt(x);
+    return y;
 }
 
 
-/** @brief Modified Bessel function of order one
+/** @brief Modified Bessel function \f$K_0(x)\f$, exponentially scaled
  *
- * Returns modified Bessel function of order one of the argument \f$x\f$.
+ * The function returns \f$\exp(x) K_0(x)\f$.
  *
- * The function is defined as \f$I_1(x) = -i J_1(ix)\f$.
+ * See \ref besselK0.
  *
- * The range is partitioned into the two intervals \f$[0,8]\f$ and \f$(8, \infty)\f$.
- * Chebyshev polynomial expansions are employed in each interval.
+ * @param [in] x argument
+ * @retval K0exp \f$\exp(x) K_0(x)\f$
+ */
+double besselK0e(double x)
+{
+    double y;
+
+    if(x <= 0)
+        return NAN;
+
+    if(x <= 2)
+    {
+        y = x*x-2.0;
+        y = chbevl(y, K0_A, 10)-log(0.5*x)*besselI0(x);
+        return(y*exp(x));
+    }
+
+    y = chbevl(8.0/x-2.0, K0_B, 25)/sqrt(x);
+
+    return y;
+}
+
+/** @brief Modified Bessel function \f$I_1(x)\f$
+ *
+ * The range is partitioned into the two intervals \f$[0,8]\f$ and \f$(8,
+ * \infty)\f$. Chebyshev polynomial expansions are employed in each interval.
  *
  * @param [in] x argument
  * @retval I1 \f$I_1(x)\f$
  */
 double besselI1(double x)
-{ 
+{
     double z = fabs(x);
 
     if(z <= 8.0)
     {
         double y = (z/2.0)-2.0;
-        z = chbevl(y,A1,29)*z*exp(z);
+        z = chbevl(y,I1_A,29)*z*exp(z);
     }
     else
-        z = exp(z)*chbevl(32.0/z-2.0,B1,25)/sqrt(z);
+        z = exp(z)*chbevl(32.0/z-2.0,I1_B,25)/sqrt(z);
     if(x < 0)
         z = -z;
     return z;
 }
 
-/** @brief Modified Bessel function of order one, exponentially scaled
+/** @brief Modified Bessel function \f$I_1(x)\f$, exponentially scaled
  *
- * Returns exponentially scaled modified Bessel function of order one of the
- * argument \f$x\f$.
- *
- * The function is defined as \f$\tilde I_1(x) = -i \exp(-|x|) J_1(ix)\f$.
+ * The function returns \f$\exp(-|x|) I_1(x)\f$.
  *
  * See \ref besselI1.
  *
@@ -300,21 +448,73 @@ double besselI1(double x)
  * @retval I1e \f$\exp(-|x|) I_1(x)\f$
  */
 double besselI1e(double x)
-{ 
+{
     double z = fabs(x);
 
     if(z <= 8.0)
     {
         double y = (z/2.0)-2.0;
-        z = chbevl(y,A1,29)*z;
+        z = chbevl(y,I1_A,29)*z;
     }
     else
-        z = chbevl(32.0/z-2.0,B1,25)/sqrt(z);
+        z = chbevl(32.0/z-2.0,I1_B,25)/sqrt(z);
 
     if(x < 0.0)
         z = -z;
 
     return z;
+}
+
+/** @brief Modified Bessel function \f$K_1(x)\f$
+ *
+ * The range is partitioned into the two intervals \f$[0,2]\f$ and \f$(2,
+ * \infty)\f$. Chebyshev polynomial expansions are employed in each interval.
+ *
+ * @param [in] x argument
+ * @retval K1 \f$K_1(x)\f$
+ */
+double besselK1(double x)
+{
+    double y, z;
+
+    z = 0.5*x;
+    if(z <= 0)
+        return NAN;
+
+    if(x <= 2.0)
+    {
+        y = x*x-2.0;
+        y =  log(z)*besselI1(x)+chbevl(y, K1_A, 11)/x;
+        return y;
+    }
+
+    return exp(-x)*chbevl(8.0/x-2.0, K1_B, 25)/sqrt(x);
+}
+
+/** @brief Modified Bessel function \f$K_1(x)\f$, exponentially scaled
+ *
+ * The function returns \f$\exp(x) K_1(x)\f$.
+ *
+ * See \ref besselK1.
+ *
+ * @param [in] x argument
+ * @retval K1exp \f$\exp(x) K_1(x)\f$
+ */
+double besselK1e(double x)
+{
+    double y;
+
+    if(x <= 0)
+        return NAN;
+
+    if(x <= 2)
+    {
+        y = x*x-2.0;
+        y = log(0.5*x)*besselI1(x)+chbevl(y, K1_A, 11)/x;
+        return y*exp(x);
+    }
+
+    return chbevl(8.0/x-2.0, K1_B, 25)/sqrt(x);
 }
 
 #define ACC 40.0
