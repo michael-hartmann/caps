576d90809b1f4ffbda39bf684cbc654dd77c4682
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-10-23 18:59:43 +0200

    remove tests

--- src/tests/test_bessels.h
+++ src/tests/test_bessels.h
@@ -2,16 +2,10 @@
 #define BESSELS_H
 
 int test_besselI0(void);
-int test_besselI0e(void);
-
 int test_besselK0(void);
-int test_besselK0e(void);
 
 int test_besselI1(void);
-int test_besselI1e(void);
-
 int test_besselK1(void);
-int test_besselK1e(void);
 
 int test_besselI(void);
 int test_besselK(void);
