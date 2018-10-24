5eef7bafecd0fc8709d4c10a6da0e42fbf9cd7c2
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-10-24 19:11:12 +0200

    rename functions

--- src/tests/test_bessels.h
+++ src/tests/test_bessels.h
@@ -1,22 +1,22 @@
 #ifndef BESSELS_H
 #define BESSELS_H
 
-int test_besselI0(void);
-int test_bessellogI0(void);
+int test_bessel_I0(void);
+int test_bessel_logI0(void);
 
-int test_besselK0(void);
-int test_bessellogK0(void);
+int test_bessel_K0(void);
+int test_bessel_logK0(void);
 
-int test_besselI1(void);
-int test_bessellogI1(void);
+int test_bessel_I1(void);
+int test_bessel_logI1(void);
 
-int test_besselK1(void);
-int test_bessellogK1(void);
+int test_bessel_K1(void);
+int test_bessel_logK1(void);
 
-int test_besselI(void);
-int test_besselK(void);
+int test_bessel_logInu_half(void);
+int test_bessel_logKnu_half(void);
 
-int test_log_besselIn(void);
-int test_log_besselKn(void);
+int test_bessel_logIn(void);
+int test_bessel_logKn(void);
 
 #endif
