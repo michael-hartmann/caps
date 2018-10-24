29f2b1f19d35d8798913be8b23fb73b3910919d1
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-10-24 19:22:59 +0200

    rename

--- src/tests/test_logi.c
+++ src/tests/test_logi.c
@@ -8,7 +8,7 @@
 int test_logi(void)
 {
     unittest_t test;
-    unittest_init(&test, "logi", "Test log for integer arguments", 1e-15);
+    unittest_init(&test, "logi", "logarithm of integers", 1e-15);
 
     for(int i = 1; i < 100000001; i++)
         AssertAlmostEqual(&test, logi(i), log(i));
