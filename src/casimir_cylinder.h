40f680530aa4850b8acea3ce36558ad19c3fb3d7
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-10-30 09:07:29 +0100

    be more verbose, add option to set detalg

--- src/casimir_cylinder.h
+++ src/casimir_cylinder.h
@@ -1,12 +1,15 @@
 #ifndef __CASIMIR_CYLINDER
 #define __CASIMIR_CYLINDER
 
+#include "matrix.h"
+
 typedef struct {
     double R; /* radius of the cylinder */
     double d; /* smallest separation beteween cylinder and plate */
     double H; /* a+R*/
     int lmax; /* truncation of vector space */
     int verbose;
+    detalg_t detalg;
 } casimir_cp_t;
 
 typedef struct {
@@ -24,6 +27,9 @@ int casimir_cp_set_lmax(casimir_cp_t *self, int lmax);
 
 int casimir_cp_get_verbose(casimir_cp_t *self);
 int casimir_cp_set_verbose(casimir_cp_t *self, int verbose);
+
+detalg_t casimir_cp_get_detalg(casimir_cp_t *self);
+int casimir_cp_set_detalg(casimir_cp_t *self, detalg_t detalg);
 
 kernel_args_t *kernel_init(casimir_cp_t *self, double q, char DN);
 void kernel_free(kernel_args_t *args);
