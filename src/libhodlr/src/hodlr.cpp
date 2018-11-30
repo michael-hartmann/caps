878f3ac12bfd14b93e7a3e999c29fdef16511d53
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2018-11-30 12:30:20 +0100

    add comment

--- src/libhodlr/src/hodlr.cpp
+++ src/libhodlr/src/hodlr.cpp
@@ -29,8 +29,11 @@ public:
 
 double hodlr_logdet(int dim, double (*callback)(int,int,void *), void *args, unsigned int nLeaf, double tolerance, int is_symmetric)
 {
-    // XXX
-    int n_levels = 1+log2(((double)dim)/nLeaf);
+    /* nLeaf is the size (number of rows of the matrix) of the smallest block
+     * at the leaf level. The number of levels in the tree is given by
+     * n_levels=log_2(N/nLeaf) where N denotes the dimension of the matrix.
+     */
+    const int n_levels = fmax(1,log2(((double)dim)/nLeaf));
 
     Kernel K = Kernel(dim, callback, args);
 
