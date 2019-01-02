c4304b0ba47eae9a805bed2b93eeab9a65588922
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2019-01-02 11:20:35 +0100

    document members of material_t

--- src/include/material.h
+++ src/include/material.h
@@ -5,14 +5,19 @@
 extern "C" {
 #endif
 
+/** material_t data type */
 typedef struct {
-    char filename[512];
-    double calL;
-    double omegap_low, gamma_low;
-    double omegap_high, gamma_high;
-    double xi_min, xi_max;
-    size_t points;
-    double *xi, *epsm1;
+    char filename[512]; /**< material filename or \0\0... */
+    double calL;        /**< \f$L+R\f$ */
+    double xi_min;      /**< lower border of tabulated frequencies */
+    double xi_max;      /**< upper border of tabulated frequencies */
+    size_t points;      /**< number of points */
+    double *xi;         /**< tabulated frequencies \f$\xi\f$ */
+    double *epsm1;      /**< tabulated dielectric function, \f$\epsilon(\mathrm{i}\xi)-1\f$ */
+    double omegap_low;  /**< plasma frequency for low frequency extrapolation */
+    double gamma_low;   /**< relaxation frequency for low frequency extrapolation */
+    double omegap_high; /**< plasma frequency for high frequency extrapolation */
+    double gamma_high;  /**< relaxation frequency for hight frequency extrapolation */
 } material_t;
 
 material_t *material_init(const char *filename, double calL);
