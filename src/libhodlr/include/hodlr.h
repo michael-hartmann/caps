dc8fd17fff68139fca06149f9dd1cb0a340b87fc
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2019-01-04 13:20:22 +0100

    improve doxygen documentation

--- src/libhodlr/include/hodlr.h
+++ src/libhodlr/include/hodlr.h
@@ -1,3 +1,9 @@
+/**
+ * @file   hodlr.h
+ * @date   January, 2019
+ * @brief  C wrapper for HODLR library
+*/
+
 #ifndef HODLR_H
 #define HODLR_H
 
@@ -7,28 +13,28 @@
 #define EXTERNC
 #endif
 
-/* @brief Calculate log(det(Id-M)) using HODLR approach
+/** @brief Calculate \f$\log\mathrm{det}(1-M)\f$ using HODLR approach
  *
  * @param dim           dimension of matrix M
  * @param callback      function that returns matrix elements of M
- * @param args          pointer that is passed to callback
+ * @param args          pointer that is passed as third argument to callback
  * @param diagonal      array with the diagonal elements of M
  * @param nLeaf         nLeaf is the dimension of the smallest block at the leaf level
  * @param tolerance     requested accuracy of result
  * @param is_symmetric  matrix is symmetric (1) or not symmetric (0)
- * @retval log(det(Id-M))
+ * @retval logdet       \f$\log\mathrm{det}(1-M)\f$
  */
 EXTERNC double hodlr_logdet_diagonal(int dim, double (*callback)(int,int,void *), void *args, double *diagonal, unsigned int nLeaf, double tolerance, int is_symmetric);
 
-/* @brief Calculate log(det(Id-M)) using HODLR approach
+/** @brief Calculate log(det(Id-M)) using HODLR approach
  *
  * @param dim           dimension of matrix M
  * @param callback      function that returns matrix elements of M
- * @param args          pointer that is passed to callback
- * @param nLeaf         ???
+ * @param args          pointer that is passed as third argument to callback
+ * @param nLeaf         nLeaf is the dimension of the smallest block at the leaf level
  * @param tolerance     requested accuracy of result
  * @param is_symmetric  matrix is symmetric (1) or not symmetric (0)
- * @retval log(det(Id-M))
+ * @retval logdet       \f$\log\mathrm{det}(1-M)\f$
  */
 EXTERNC double hodlr_logdet(int dim, double (*callback)(int,int,void *), void *args, unsigned int nLeaf, double tolerance, int is_symmetric);
 
