c039e657ddf20852287c25bebe81c2dd62afb6d9
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2019-01-07 11:08:38 +0100

    update

--- materials/plot.py
+++ materials/plot.py
@@ -3,14 +3,10 @@ import matplotlib.pyplot as plt
 
 hbar = 6.582119514e-16 # eV*s/rad
 
-dalvit = np.loadtxt("GoldDalvit.dat")
-gold   = np.loadtxt("GoldPalik.dat")
-palik  = np.loadtxt("GoldPalik.dat")
+gold   = np.loadtxt("gold.csv")
 
 plt.xlabel("ξ in rad/s")
 plt.ylabel("ε(iξ)")
 plt.loglog(gold[:,0], gold[:,1])
-plt.loglog(dalvit[:,0], dalvit[:,1])
-plt.loglog(palik[:,0], palik[:,1])
 
 plt.show()
