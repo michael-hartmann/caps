03d8a006e46fddb3b45a6da0f9d336fd2b1fee29
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2019-02-26 17:51:48 +0100

    simplify PyX script

--- docs/paper/geometry.py
+++ docs/paper/geometry.py
@@ -1,6 +1,4 @@
-from math import sqrt, pi, sin, cos, radians
-
-import numpy as np
+from math import sin, cos, radians
 from pyx import *
 
 R = 1
@@ -26,21 +24,11 @@ c.stroke(path.line(0, -R-L, 0, 1.5), [ deco.earrow([deco.stroked([deco.stroked.c
 
 # line and label for radius R (45°)
 c.insert(text.text(0.45, -0.05, r"$R$", [text.halign.center]))
-#c.stroke(path.line(0, 0, 1/sqrt(2), -1/sqrt(2)), [deco.earrow.small])
 phi = radians(-25)
 c.stroke(path.line(0, 0, cos(phi), sin(phi)), [deco.earrow.small, style.linewidth.thick])
-
-# the two dashed lines
-#c.stroke(path.line(-1.2, -1, 1.19, -1), [style.linestyle.dashed])
-#c.stroke(path.line(-0.8, 0, 0.8, 0), [style.linestyle.dashed])
-#c.fill(path.circle(0, 0, 0.04))
 
 # line with arrows and label L
 c.insert(text.text(-0.34, -R-L/2, r"$L$", [text.halign.flushcenter, text.valign.middle]))
 c.stroke(path.line(0, -R-L, 0, -1), [deco.barrow.small, deco.earrow.small, style.linewidth.thick])
-
-# line with arrows and label calL
-#c.insert(text.text(1.5, -0.9, r"$R+L$"))
-#c.stroke(path.line(1.4, -2, 1.4, 0), attr)
 
 c.writePDFfile()
