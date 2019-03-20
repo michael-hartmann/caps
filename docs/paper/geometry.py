907cbf69d4fe54638dfb05c26b247c3f9b97da04
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2019-03-20 18:11:46 +0100

    minor changes on the figure

--- docs/paper/geometry.py
+++ docs/paper/geometry.py
@@ -6,26 +6,23 @@ L = 1/2
 
 text.set(text.LatexRunner)
 
-attr = [deco.barrow.small, deco.earrow.small]
-
 c = canvas.canvas()
-fillstyle = [style.linewidth.Thin, color.rgb.black, deco.filled([color.grey(0.8)])]
-mypainter=graph.axis.painter.regular(labeldist=0.15*unit.v_cm)
+fillstyle = [style.linewidth.thin, color.rgb.black, deco.filled([color.grey(0.8)])]
 
 # sphere
 c.stroke(path.circle(0, 0, R), fillstyle)
 
 # plane
-c.stroke(path.rect(-1.3, -R-L, 2.6, -0.1), fillstyle)
+c.stroke(path.rect(-2, -R-L, 4, -0.15), fillstyle)
 
 # z-axis
 c.insert(text.text(0.17, 1.35, r"$z$"))
-c.stroke(path.line(0, -R-L, 0, 1.5), [ deco.earrow([deco.stroked([deco.stroked.clear])])])
+c.stroke(path.line(0, -R-L, 0, 1.5), [deco.earrow])
 
-# line and label for radius R (45°)
+# line and label for radius R (25°)
 c.insert(text.text(0.45, -0.05, r"$R$", [text.halign.center]))
 phi = radians(-25)
-c.stroke(path.line(0, 0, cos(phi), sin(phi)), [deco.earrow.small, style.linewidth.thick])
+c.stroke(path.line(0, 0, cos(phi), sin(phi)), [deco.earrow, style.linewidth.thick])
 
 # line with arrows and label L
 c.insert(text.text(-0.34, -R-L/2, r"$L$", [text.halign.flushcenter, text.valign.middle]))
