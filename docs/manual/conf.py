b7ecd9a7282aadf15b0d2e04f918a8a84b12b50c
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2019-06-26 07:45:05 +0200

    converted to a4 paper

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -109,7 +109,7 @@ htmlhelp_basename = 'capsdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
