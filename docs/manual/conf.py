88610da532b03aa49284764b477fedba51bd0ee5
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2019-06-26 07:43:47 +0200

    avoid blank pages

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -122,6 +122,8 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
+
+    'extraclassoptions': 'openany',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
