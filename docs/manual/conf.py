45aa66cd98850f65c30045ae91992cfbfeb9ba7d
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2019-07-03 09:26:04 +0200

    tex version of manual renamed

--- docs/manual/conf.py
+++ docs/manual/conf.py
@@ -129,7 +129,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'caps.tex', 'CaPS Documentation',
+    (master_doc, 'manual.tex', 'CaPS Documentation',
      'Michael Hartmann, Gert-Ludwig Ingold', 'manual'),
 ]
 
